81b81a88171d37bc296ba63b5407b506e5fd5f8b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-04-30 17:48:37 -0700

    Included in the file the configuration, the header, and added a page title

---
+++ register.php
@@ -0,0 +1,18 @@
+<?php
+
+# register.php.
+# Created on April 30, 2019.
+# Created by Elias Gutierrez.
+
+// This file is the registration page for the website.
+
+// Include the configuration script (this would be used to call some of the variables and the error management):
+require('includes/config.inc.php');
+
+// Including the page title:
+$page_title = 'Register';
+
+// Include the header:
+include('includes/header.html');
+
+?>
