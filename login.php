0fbe17276f5bff4ce3a6d1f6894c544110b09edf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-05-01 14:16:14 -0700

    Added the config file, page title, and header file on lines 10, 13, and 16

---
+++ login.php
@@ -0,0 +1,18 @@
+<?php
+
+# login.php.
+# Created on May 1, 2019.
+# Created by Elias Gutierrez.
+
+// This file is the login page for the user to login if registered.
+
+// Include the configuration file which would be used to call some of the variables and the error management:
+require('includes/config.inc.php');
+
+// Add the proper page title:
+$page_title = 'Login';
+
+// Include the header file:
+include('includes/header.html');
+
+?>
