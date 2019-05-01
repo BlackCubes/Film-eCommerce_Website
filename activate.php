7484c62984a2de01e01e6f5886a3c79611a969ff
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-05-01 12:24:23 -0700

    Added the configuration file, page title, and header file on lines 10, 13, and 16

---
+++ activate.php
@@ -0,0 +1,18 @@
+<?php
+
+# activate.php.
+# Created on May 1, 2019.
+# Created by Elias Gutierrez.
+
+// This file activates the user's account after they have registered and clicked on the activation link through their email.
+
+// Include the configuration file (this would be used to call some of the variables and the error management):
+require('includes/config.inc.php');
+
+// Include the page title:
+$page_title = 'Activate Your Account';
+
+// Include the header file:
+include('includes/header.html');
+
+?>
