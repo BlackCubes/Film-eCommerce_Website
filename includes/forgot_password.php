2fe54472e0ea99362740215a31875daf7fb20a40
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-05-02 10:30:59 -0700

    Added the config file, page title, and header file on lines 10, 13, and 16

---
+++ includes/forgot_password.php
@@ -0,0 +1,18 @@
+<?php
+
+# forgot_password.php.
+# Created on May 2, 2019.
+# Created by Elias Gutierrez.
+
+// This file would send the user a temporary password to their email to use to log in if they are registered in the system.
+
+// Including the configuration file which would use some of the variables and the error management:
+require('includes/config.inc.php');
+
+// Naming the proper page title:
+$page_title = 'Forgot Password';
+
+// Include the header file:
+include('includes/header.html');
+
+?>
