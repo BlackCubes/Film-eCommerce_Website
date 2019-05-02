ed8e924adb0f6cbb80b566cab64b00466bb552a8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-05-01 19:36:17 -0700

    Included the config file and header file, and named a page title on lines 10, 13, and 16

---
+++ logout.php
@@ -0,0 +1,18 @@
+<?php
+
+# logout.php.
+# Created on May 1, 2019.
+# Created by Elias Gutierrez.
+
+// This file is the logout page for the user to logout if he is logged in. If the user has not logged in, the option for logging out will not appear.
+
+// Including the configuration file which would be used to call some of the variables and the error management:
+require('includes/config.inc.php');
+
+// Including the proper title for this page:
+$page_title = 'Logout';
+
+// Including the header file:
+include('includes/header.html');
+
+?>
