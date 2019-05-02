10826d41695f4dcc64ca49a8df67fde62a27e52c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-05-02 10:56:18 -0700

    Added the database connection and assumed a variable is false on lines 22 and 25

--- includes/forgot_password.php
+++ includes/forgot_password.php
@@ -15,4 +15,14 @@ $page_title = 'Forgot Password';
 // Include the header file:
 include('includes/header.html');
 
+// This is the main Submit Conditional after the user submits using the POST method:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    // Include the database connection which the variable was defined on line 35 in the file config.inc.php:
+    require(MYSQL);
+
+    // The variable $uid (which is the user's id) is set to FALSE assuming that the particular individual has not logged in yet. This variable will be used later:
+    $uid = FALSE;
+}
+
 ?>
