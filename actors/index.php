61f1ddb3feb9258531e3068d21402841b640a08c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-01 10:51:34 -0700

    Added pagination on lines 74-96

--- actors/index.php
+++ actors/index.php
@@ -71,6 +71,30 @@ echo '</div></div>
 mysqli_free_result($r);
 mysqli_close($dbc);
 
+if ($pages > 1) {
+
+    echo '<br><p>';
+    $current_page = ($start/$display) + 1;
+
+    if ($current_page != 1) {
+        echo '<a href="index.php?s=' . ($start - $display) . '&p=' . $pages . '">Previous</a>';
+    }
+
+    for ($i = 1; $i <= $pages; $i++) {
+        if ($i != $current_page) {
+            echo '<a href="index.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '">' . $i . '</a>';
+        } else {
+            echo $i . ' ';
+        }
+    }
+
+    if ($current_page != $pages) {
+        echo '<a href="index.php?s=' . ($start + $display) . '&p=' . $pages= . '">Next</a>';
+    }
+
+    echo '</p>';
+}
+
 ?>
 
 <div class="container-fluid">
