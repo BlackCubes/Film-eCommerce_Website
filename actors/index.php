a361ed0af210c3b08ec31754bff5dc20f6117d88
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-01 12:25:42 -0700

    Fixing URL issue

--- actors/index.php
+++ actors/index.php
@@ -15,7 +15,7 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
-require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+require_once($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
 
 $page_title = 'Actors';
 
