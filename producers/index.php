62c3ea23b11b60e8d932846e94870aa33c4bcb10
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-02 20:57:17 -0700

    Fixed syntax error on line 78

--- producers/index.php
+++ producers/index.php
@@ -75,7 +75,7 @@ echo '<div class="container-fluid">
         <p><a href="index.php?sort=m">Male</a></p>
     </div>
     <div class="col-8 inline" id="producersCollection">
-        <div class="row grid grid--view-items">'
+        <div class="row grid grid--view-items">';
 
 echo '</div></div>
 <div class="col-2"></div>
