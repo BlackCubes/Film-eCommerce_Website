fb5e902b2992fbc245b7c3b263db270a6dc420c5
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-12 18:06:29 -0700

    Initialized and added documentation on lines 3-15

---
+++ suppliers/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Suppliers Homepage).
+# Created June 12, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
