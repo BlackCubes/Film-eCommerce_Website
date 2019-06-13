4556e33052616f69b06ace4e69a64c8334abf9e2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-12 18:12:56 -0700

    Copied main section of suppliers index html and included the footer file on lines 22, 24

--- suppliers/index.php
+++ suppliers/index.php
@@ -18,3 +18,7 @@ $page_title = 'Suppliers Central Hub';
 include(__DIR__.'\includes\header.html');
 
 ?>
+
+<h1>Main Supplier's Home Content</h1>
+
+<?php include(__DIR__'\includes\footer.html'); ?>
