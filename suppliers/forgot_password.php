4d0d7d17135e4ca889efe78d0bb68259e5cdb37e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 13:50:15 -0700

    Added and initialized documentation on lines 3-15

---
+++ suppliers/forgot_password.php
@@ -0,0 +1,17 @@
+<?php
+
+# forgot_password.php (Suppliers forgotten password).
+# Created on June 14, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
