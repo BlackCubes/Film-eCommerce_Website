983585c78292ada8e6646039779b283c73640704
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 13:52:01 -0700

    Added config and header files, and page title on lines 17-21

--- suppliers/forgot_password.php
+++ suppliers/forgot_password.php
@@ -14,4 +14,10 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+$page_title = 'Forgot Password';
+
+include('includes/header.html');
+
 ?>
