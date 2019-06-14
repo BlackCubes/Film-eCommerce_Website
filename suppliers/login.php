850f275e815d8c4bbb43560838b4393df7548ab9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 11:40:54 -0700

    Added html login on lines 22-30

--- suppliers/login.php
+++ suppliers/login.php
@@ -19,5 +19,14 @@
 <!--
 
 -->
+<h1>Login</h1>
+<p>Your browser must allow cookies in order to log in.</p>
+<form action="login.php" method="post">
+    <fieldset>
+        <p><strong>Email Address:</strong> <input type="email" name="email" size="20" maxlength="80"></p>
+        <p><strong>Password:</strong> <input type="password" name="pass" size="20"></p>
+        <div align="center"><input type="submit" name="submit" value="Login"></div>
+    </fieldset>
+</form>
 
 <?php include('includes/footer.html'); ?>
