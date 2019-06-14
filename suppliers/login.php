9bbfbb1917ad9be03ad83617a76ae1c726dfd75d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 11:07:56 -0700

    Created and initialized documentation on lines 3-15

---
+++ suppliers/login.php
@@ -0,0 +1,17 @@
+<?php
+
+# login.php (Supplier's login).
+# Created on June 14, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
