4f15947a01c59e9ba98f8299f5d26f7ec1c97b68
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 12:56:56 -0700

    Added first if-conditional using GET method on line 23

--- suppliers/activate.php
+++ suppliers/activate.php
@@ -20,4 +20,8 @@ $page_title = 'Activate Your Account';
 
 include('includes/header.html');
 
+if (isset($_GET['x'], $_GET['y']) && filter_var($_GET['x'], FILTER_VALIDATE_EMAIL) && (strlen($_GET['y']) == 32)) {
+    
+}
+
 ?>
