2c7a70699a867ab943285da43dcf307ea1803e43
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 13:03:57 -0700

    Added second if-conditional on lines 29-33

--- suppliers/activate.php
+++ suppliers/activate.php
@@ -26,6 +26,12 @@ if (isset($_GET['x'], $_GET['y']) && filter_var($_GET['x'], FILTER_VALIDATE_EMAI
     $q = "UPDATE suppliers SET verify_code=NULL WHERE (email='" . mysqli_real_escape_string($dbc, $_GET['x']) . "' AND verify_code='" . mysqli_real_escape_string($dbc, $_GET['y']) . "') LIMIT 1";
     $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
+    if (mysqli_affected_rows($dbc) == 1) {
+        echo "<h3>Your account is not active! You may log in.</h3>";
+    } else {
+        echo '<p class="error">Sorry, but your account could not be activated. Please re-check the link given to you in your email address, or contact the system administrator.</p>';
+    }
+
 }
 
 ?>
