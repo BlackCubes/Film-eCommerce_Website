0198f6e814bc45d3fee6b7312df216cabe61f281
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 13:18:30 -0700

    Added and initialized documentation on lines 3-15

---
+++ suppliers/change_password.php
@@ -0,0 +1,17 @@
+<?php
+
+# change_password.php (Suppliers password change).
+# Created on June 14, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
