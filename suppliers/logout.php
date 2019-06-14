cd3f05aca4e3e7c3ea7ca51c3b7e6a3f737ecae1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 12:30:28 -0700

    Created and initialized documentation on lines 3-15

---
+++ suppliers/logout.php
@@ -0,0 +1,17 @@
+<?php
+
+# logout.php (Suppliers logout).
+# Created on June 14, 2019.
+# Created by Elias Gutierrezz.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
