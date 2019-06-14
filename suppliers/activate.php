9093d1429b646165f6ec8197e8bc0fa283d518cb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-14 13:01:26 -0700

    Called DB connection, and added query on lines 25-27

--- suppliers/activate.php
+++ suppliers/activate.php
@@ -21,7 +21,11 @@ $page_title = 'Activate Your Account';
 include('includes/header.html');
 
 if (isset($_GET['x'], $_GET['y']) && filter_var($_GET['x'], FILTER_VALIDATE_EMAIL) && (strlen($_GET['y']) == 32)) {
-    
+
+    require(MYSQL);
+    $q = "UPDATE suppliers SET verify_code=NULL WHERE (email='" . mysqli_real_escape_string($dbc, $_GET['x']) . "' AND verify_code='" . mysqli_real_escape_string($dbc, $_GET['y']) . "') LIMIT 1";
+    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
 }
 
 ?>
