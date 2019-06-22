02317fbf4567a114274dbd9e48ea48af863f4efc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-06-22 15:48:50 -0700

    Removed some php

--- suppliers/TEST_add_products.php
+++ suppliers/TEST_add_products.php
@@ -2,60 +2,56 @@
 
 require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+require(MYSQL);
 
-    require(MYSQL);
+$trimmed = array_map('trim', $_POST);
 
-    $trimmed = array_map('trim', $_POST);
-    
-    //NOTE: Also need to verify if they exist in DB!
-    //NOTE: Might have to create another if to see if it matches the requirements before DB-if!
-    if (isset($trimmed['directors_first_name'])) {
-    
-        $q_directors = "SELECT first_name FROM directors";
-        $r_directors = mysqli_query($dbc, $q_directors) or trigger_error("Query: $q_directors\n<br>MySQL error: " . mysqli_error($dbc));
-        $directors_preg = mysqli_fetch_all($r_directors, MYSQLI_ASSOC);
-        $dp_first_name = array();
-        while ($rows = array_shift($dp_first_name)) {
-            $dp_first_name[array_shift($rows)] = $rows;
-        }
-        echo $dp_first_name;
+//NOTE: Also need to verify if they exist in DB!
+//NOTE: Might have to create another if to see if it matches the requirements before DB-if!
+if (isset($trimmed['directors_first_name'])) {
 
-        $d_fn = preg_split('/[\s,]+/', $trimmed['directors_first_name']);
-        #$d_ln = preg_split('/[\s,]+/', $trimmed['directors_last_name']);
-    
-        #foreach ($dp_first_name as $dp_fn) {
-            #if ($matches1=preg_grep('/\b($dp_fn)\b/', $d_fn)) {
-                #echo '<p>Success!</p>';
-    
-                #foreach ($d_fn as &$value1) {
-                #    $value1 = $value1 . '%';
-                #}
-                #unset($value1);
-                #foreach ($d_ln as &$value2) {
-                #    $value2 = $value2 . '%';
-                #}
-                #unset($value2);
-    
-                //NOTE: Might have to create query array!
-                //NOTE: Fix the for loop?
-                #for ($f = 0, $l = 0; $f < sizeof($d_fn), $l < sizeof($d_ln); $f++, $l++) {
-                #    $q_did = "SELECT id FROM directors WHERE first_name LIKE ' . $d_fn[i] . ' AND last_name LIKE ' . $d_ln[j] . '";
-                #}
-                #echo '<pre>Success!</pre>';
-    
-            #} else {
-                #echo '<p>Error, or none!</p>';
-            #}
-        #}
-    
-    } else {
-        echo '<p>Did not match the preg verification, or did not input at least one individual!</p>';
+    $q_directors = "SELECT first_name FROM directors";
+    $r_directors = mysqli_query($dbc, $q_directors) or trigger_error("Query: $q_directors\n<br>MySQL error: " . mysqli_error($dbc));
+    $directors_preg = mysqli_fetch_all($r_directors, MYSQLI_ASSOC);
+    $dp_first_name = array();
+    while ($rows = array_shift($dp_first_name)) {
+        $dp_first_name[array_shift($rows)] = $rows;
     }
+    echo $dp_first_name;
 
-    mysqli_close($dbc);
+    $d_fn = preg_split('/[\s,]+/', $trimmed['directors_first_name']);
+    #$d_ln = preg_split('/[\s,]+/', $trimmed['directors_last_name']);
 
+    #foreach ($dp_first_name as $dp_fn) {
+        #if ($matches1=preg_grep('/\b($dp_fn)\b/', $d_fn)) {
+            #echo '<p>Success!</p>';
+
+            #foreach ($d_fn as &$value1) {
+            #    $value1 = $value1 . '%';
+            #}
+            #unset($value1);
+            #foreach ($d_ln as &$value2) {
+            #    $value2 = $value2 . '%';
+            #}
+            #unset($value2);
+
+            //NOTE: Might have to create query array!
+            //NOTE: Fix the for loop?
+            #for ($f = 0, $l = 0; $f < sizeof($d_fn), $l < sizeof($d_ln); $f++, $l++) {
+            #    $q_did = "SELECT id FROM directors WHERE first_name LIKE ' . $d_fn[i] . ' AND last_name LIKE ' . $d_ln[j] . '";
+            #}
+            #echo '<pre>Success!</pre>';
+
+        #} else {
+            #echo '<p>Error, or none!</p>';
+        #}
+    #}
+
+} else {
+    echo '<p>Did not match the preg verification, or did not input at least one individual!</p>';
 }
+
+mysqli_close($dbc);
 ?>
 
 <!--<form action="TEST_add_products.php" method="post">
