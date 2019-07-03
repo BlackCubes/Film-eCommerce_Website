82394faf6c3d1a1e762aa58ed158afa952b0b7a9
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-03 10:46:40 -0700

    Initialized info and documentation

---
+++ suppliers/upload_images.php
@@ -0,0 +1,17 @@
+<?php
+
+# upload_images.php (Suppliers uploading images to products).
+# Created by Elias Gutierrez.
+# Created on July 3, 2019.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
