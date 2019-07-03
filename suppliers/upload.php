b4f18745d767e9e511be6aad817b6dd4005d336d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-03 11:43:59 -0700

    Added variables to get info on file uploads

--- suppliers/upload.php
+++ suppliers/upload.php
@@ -17,7 +17,19 @@
 require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/suppliers/includes/config.inc.php');
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+
+    $file = $_FILES['upload_file'];
+
+    $file_name = $_FILES['upload_file']['name'];
+
+    $file_tmp_name = $_FILES['upload_file']['tmp_name'];
+
+    $file_size = $_FILES['upload_file']['size'];
+
+    $file_error = $_FILES['upload_file']['error'];
+
+    $file_type = $_FILES['upload_file']['type'];
+
 }
 
 ?>
