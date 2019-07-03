c85cf2e5d033112d84e82eda30b19801693d2ee1
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-03 11:14:48 -0700

    Added and initialized info and documentation

---
+++ suppliers/upload.php
@@ -0,0 +1,17 @@
+<?php
+
+# upload.php (Upload Form Handler).
+# Created by Elias Gutierrez.
+# Created on July 3, 2019.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
