e20d653c13b62eeccd58b4c93945a7ebdfdc3f3c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-06 14:57:05 -0700

    Added and initialized info and documentation

---
+++ films/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Users/Customers Shop for Films page).
+# Created by Elias Gutierrez.
+# Created on July 6, 2019.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
