15420f2d8da188824ad2695c3c295504add3ed4a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-06 15:01:15 -0700

    Added files of config, footer, and header

--- films/index.php
+++ films/index.php
@@ -14,4 +14,10 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+
 ?>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
