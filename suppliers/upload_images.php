d8276b7d68e333a12043631d17e0782c14b8ddcd
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-05 19:44:13 -0700

    Reformatted by adding if-conditional with error messages

--- suppliers/upload_images.php
+++ suppliers/upload_images.php
@@ -63,14 +63,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $file_new_name_1 = uniqid('', TRUE) . '.' . $file_real_ext_1;
                 $file_new_name_2 = uniqid('', TRUE) . '.' . $file_real_ext_2;
 
-                $q = "INSERT INTO products AS p (image_1, image_2) VALUES ('{$file_new_name_1}', '{$file_new_name_2}') JOIN suppliers_products AS sp ON p.id=sp.product_id JOIN suppliers AS s ON sp.supplier_id=s.id AND s.id={$_SESSION['id']} AND p.id=(SELECT id FROM p WHERE isd={$_POST['isd']})";
-                $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_close($dbc));
-
                 $file_destination_1 = $_SERVER['DOCUMENT_ROOT'] . '/FilmIndustry/uploads/products/' . $file_new_name_1;
                 $file_destination_2 = $_SERVER['DOCUMENT_ROOT'] . '/FilmIndustry/uploads/products/' . $file_new_name_2;
 
-                move_uploaded_file($file_tmp_name_1, $file_destination_1);
-                move_uploaded_file($file_tmp_name_2, $file_destination_2);
+                if (move_uploaded_file($file_tmp_name_1, $file_destination_1) && move_uploaded_file($file_tmp_name_2, $file_destination_2)) {
+
+                    $q = "INSERT INTO products AS p (image_1, image_2) VALUES ('{$file_new_name_1}', '{$file_new_name_2}') JOIN suppliers_products AS sp ON p.id=sp.product_id JOIN suppliers AS s ON sp.supplier_id=s.id AND s.id={$_SESSION['id']} AND p.id=(SELECT id FROM p WHERE isd={$_POST['isd']})";
+
+                    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_close($dbc));
+
+                } else {
+                    echo '<p class="text-danger">Sorry, but there was an error uploading your file!</p>';
+                }
 
                 header("Location: upload_images.php");
 
