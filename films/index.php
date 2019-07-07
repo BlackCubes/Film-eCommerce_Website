7f7cbbf5f1d5129af4fda1a02c6ce0c59470e1ee
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-06 21:53:27 -0700

    Added query and result to display the products

--- films/index.php
+++ films/index.php
@@ -25,7 +25,18 @@ require(MYSQL);
 ?>
 <div class="container">
     <div class="sidebar-a"></div>
-    <div class="main-product"></div>
+    <div class="main-product">
+        <?php
+
+        $q = "SELECT p.name, f.format, p.unit_price, p.image_1 FROM products AS p JOIN formats AS f ON p.format_id=f.id ORDER BY p.name";
+        $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
+
+        while ($product = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+            echo $product['p.name'];
+        }
+
+        ?>
+    </div>
     <div class="sidebar-b"></div>
 </div>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
