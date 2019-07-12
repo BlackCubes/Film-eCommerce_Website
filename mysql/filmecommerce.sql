35c04db23ed1bd01ebc9096c569234dc0d46155f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-11 21:10:52 -0700

    Added specifications for Bourne Supremacy UHD in specs tablein DB

--- mysql/filmecommerce.sql
+++ mysql/filmecommerce.sql
@@ -1020,7 +1020,8 @@ CREATE TABLE `specs` (
 INSERT INTO `specs` (`id`, `spec_format_type`, `video_desc`, `audio_desc`, `subtitles_desc`) VALUES
 (1, '4K UHD Main Feature', '2160p UHD 16x9 2.4:1', 'DTS-HD MA: English 5.1, Dolby Digital: Français 5.1 (Dubbed in Quebec), Español 5.1', 'English SDH, Français, and Español'),
 (2, '4K ULTRA HD Main Feature', 'UHD HDR10 Widescreen 2.35:1', 'English DTS:X Immersive Audio, DTS Headphone:X; Español, Français DTS Digital Surround 5.1', 'English SDH, Français, and Español'),
-(3, '4K ULTRA HD Main Feature', 'UHD HDR10 Widescreen 2.40:1', 'English DTS:X Immersive Audio, DTS Headphone:X 2.0; Español, Français, Japanese, Brazilian Portuguese DTS Digital Surround 5.1', 'English SDH, Español, Français, Japanese, Brazilian Portuguese');
+(3, '4K ULTRA HD Main Feature', 'UHD HDR10 Widescreen 2.40:1', 'English DTS:X Immersive Audio, DTS Headphone:X 2.0; Español, Français, Japanese, Brazilian Portuguese DTS Digital Surround 5.1', 'English SDH, Español, Français, Japanese, Brazilian Portuguese'),
+(4, '4K ULTRA HD Main Feature', 'UHD HDR10 Widescreen 2.35:1', 'English DTS:X Immersive Audio, DTS Headphone:X 2.0; Español, Français, Japanese, Brazilian Portuguese DTS Digital Surround 5.1', 'English SDH, Español, Français, Japanese, Brazilian Portuguese');
 
 -- --------------------------------------------------------
 
