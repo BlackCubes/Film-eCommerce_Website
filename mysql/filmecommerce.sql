28f1b35ccbeaa82643936a97c797bf058249b506
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-12 16:11:29 -0700

    Added details for Dark Knight Rises UHD in productdetails table in DB

--- mysql/filmecommerce.sql
+++ mysql/filmecommerce.sql
@@ -687,7 +687,8 @@ INSERT INTO `productdetails` (`id`, `rated_id`, `spec_id`, `edition`, `discs`, `
 (3, 3, 3, '4K Ultra HD+Blu-ray+Digital', 2, 135, '2016-12-06', ''),
 (4, 3, 4, '4K Ultra HD+Blu-ray+Digital', 2, 108, '2016-12-06', ''),
 (5, 3, 5, 'Standard Edition', 3, 152, '2017-12-19', ''),
-(6, 3, 6, '4K Ultra HD+Blu-ray+Digital', 2, 115, '2016-12-06', '');
+(6, 3, 6, '4K Ultra HD+Blu-ray+Digital', 2, 115, '2016-12-06', ''),
+(7, 3, 7, 'Standard Edition', 3, 165, '2017-12-19', '');
 
 -- --------------------------------------------------------
 
