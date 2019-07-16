bad0d5641f870fa4ce8f9e493579be53a118e42d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-16 16:16:55 -0700

    Added 1 director in directors table in DB

--- mysql/filmecommerce.sql
+++ mysql/filmecommerce.sql
@@ -381,7 +381,8 @@ INSERT INTO `directors` (`id`, `first_name`, `middle_name`, `last_name`, `gender
 (77, 'Tony', '', 'Gilroy', 'M', '', 'directors/tony-gilroy_Cover.jpg'),
 (78, 'Paul', '', 'Greengrass', 'M', '', 'directors/paul-greengrass_Cover.jpg'),
 (79, 'Tim', '', 'Miller', 'M', '', 'directors/tim-miller_Cover.png'),
-(80, 'David', '', 'Leitch', 'M', '', 'directors/david-leitch_Cover.jpg');
+(80, 'David', '', 'Leitch', 'M', '', 'directors/david-leitch_Cover.jpg'),
+(81, 'Chris', '', 'Columbus', 'M', '', 'directors/chris-columbus_Cover.jpg');
 
 -- --------------------------------------------------------
 
