584ff9a0e9fa20f78b05c316e8ad7f88f3c5ea8f
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-17 14:07:11 -0700

    Added specifications for HP5 4K in specs table in DB

--- mysql/filmecommerce.sql
+++ mysql/filmecommerce.sql
@@ -1537,7 +1537,8 @@ INSERT INTO `specs` (`id`, `spec_format_type`, `video_desc`, `audio_desc`, `subt
 (10, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.4:1', 'DTS:X English, English Descriptive Autdio 2.0, Dolby Digital: Español 5.1 EX', 'English SDH &amp; Español'),
 (11, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.4:1', 'DTS:X English, English Descriptive Audio 5.1 EX, Dolby Digital: Français 5.1 EX (Dubbed in Quebec) &amp; Español 5.1 EX (Both Castilian &amp; Latin)', 'English SDH, Français, &amp; Español'),
 (12, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.41:1', 'DTS:X English, English Descriptive Audio 5.1 EX, Dolby Digital: Français 5.1 (Dubbed in Quebec) &amp; Español 5.1 EX (Both Castilian &amp; Latin)', 'English SDH, Français, &amp; Español'),
-(13, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.4:1', 'DTS:X English, English Descriptive Audio 5.1, Dolby Digital: Français 5.1 (Dubbed in Quebec) &amp; Español 5.1 (Both Castilian &amp; Latin)', 'English SDH, Français, &amp; Español');
+(13, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.4:1', 'DTS:X English, English Descriptive Audio 5.1, Dolby Digital: Français 5.1 (Dubbed in Quebec) &amp; Español 5.1 (Both Castilian &amp; Latin)', 'English SDH, Français, &amp; Español'),
+(14, '4K UHD MAIN FEATURE', '2160p UHD 16x9 2.4:1', 'DTS:X English, English Descriptive Audio 2.0, Dolby Digital: Français 5.1 (Dubbed in Quebec) &amp; Español 5.1', 'English SDH, Français, &amp; Español');
 
 -- --------------------------------------------------------
 
