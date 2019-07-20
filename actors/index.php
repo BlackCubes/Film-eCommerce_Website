488e9da398dab90d8d6bacb93c773d6525f244da
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-20 12:22:37 -0700

    Added id attribute and changed one class name

--- actors/index.php
+++ actors/index.php
@@ -60,7 +60,7 @@ if (isset($_GET['s']) && is_numeric($_GET['s'])) {
     $start = 0;
 }
 
-echo '<div class="container">
+echo '<div class="container" id="artistActors">
 <div class="sidebar-a">
     <div class="results">
         <p><b>Gender</b></p>
@@ -75,7 +75,7 @@ $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mys
 echo'<div class="main-actors">';
 
 while ($actor = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-    echo '<div class="container-actors"><div class="actor-image"><img src="/FilmIndustry/eCommerce/img/' . $actor['img'] . '" alt="#"></div><div class="actor-name">' . $actor['first_name'] . ' ' . $actor['middle_name'] . ' ' . $actor['last_name'] . '</div></div>';
+    echo '<div class="container-actor"><div class="actor-image"><img src="/FilmIndustry/eCommerce/img/' . $actor['img'] . '" alt="#"></div><div class="actor-name">' . $actor['first_name'] . ' ' . $actor['middle_name'] . ' ' . $actor['last_name'] . '</div></div>';
 }
 
 mysqli_free_result($r);
