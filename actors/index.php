69f0a727499e4c0e01cb0de8a5a9040f0ea4df6d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-19 23:24:26 -0700

    Reformatted and deleted old lines of code which would include class elements for css

--- actors/index.php
+++ actors/index.php
@@ -60,61 +60,50 @@ if (isset($_GET['s']) && is_numeric($_GET['s'])) {
     $start = 0;
 }
 
-$q = "SELECT first_name, middle_name, last_name, img FROM actors $where ORDER BY last_name LIMIT $start, $display";
-$r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
-
-echo '<div class="container-fluid">
-<nav class="breadcrumb t-uppercase" role="navigation" aria-label="breadcrumbs">
-    <li><a href="' . BASE_URL . 'index.php">Home</a></li>
-    <li><a href="' . BASE_URL . 'actors/index.php">' . $page_title . '</a></li>
-</nav>
-<div class="row">
-    <div class="col-2">
-        <p><b>Gender</b></p>
-        <p><a href="index.php?sort=f">Female</a></p>
-        <p><a href="index.php?sort=m">Male</a></p>
-    </div>
-    <div class="col-8 inline" id="actorsCollection">
-        <div class="row grid grid--view-items">';
-
-while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-    echo '<div class="col grid_item">
-    <div class="grid-view-item">
-        <a class="grid-view-item_link" href="#"><img id="productImage-collection-template-1" src="' . BASE_URL . 'img/' . $row['img'] . '" alt="#"></a>
-        <a href="#"><div class="h4 grid-view-item_title">' . $row['first_name'] . ' ' . $row['middle_name'] . ' ' . $row['last_name'] . '</div></a>
-    </div>
+echo '<div class="container">
+<div class="sidebar-a">
+    <p><a href="/FilmIndustry/eCommerce/actors/index.php?sort=f">Female</a></p>
+    <p><a href="/FilmIndustry/eCommerce/actors/index.php?sort=m">Male</a></p>
 </div>';
+
+$q = "SELECT first_name, middle_name, last_name, img FROM actors $where ORDER BY last_name LIMIT $start, $display";
+$r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_close($dbc));
+
+echo'<div class="main-actors">';
+
+while ($actor = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+    echo '<div class="container-actors"><div class="actor-image"><img src="' . $actor['img'] . '" alt="#"></div><div class="actor-name">' . $actor['first_name'] . ' ' . $actor['middle_name'] . ' ' . $actor['last_name'] . '</div></div>';
 }
 
-echo '</div></div>
-<div class="col-2"></div>
-</div></div>';
 mysqli_free_result($r);
 mysqli_close($dbc);
 
 if ($pages > 1) {
 
-    echo '<br><p>';
+    echo '<div class="actor-pagination">';
     $current_page = ($start/$display) + 1;
 
     if ($current_page != 1) {
-        echo '<a href="index.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a>';
+        echo '<a href="/FilmIndustry/eCommerce/actors/index.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a>';
     }
 
     for ($i = 1; $i <= $pages; $i++) {
         if ($i != $current_page) {
-            echo '<a href="index.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a>';
+            echo '<a href="/FilmIndustry/eCommerce/actors/index.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a>';
         } else {
             echo $i . ' ';
         }
     }
 
     if ($current_page != $pages) {
-        echo '<a href="index.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">Next</a>';
+        echo '<a href="/FilmIndustry/eCommerce/actors/index.php?s=' . ($start + $display) .'&p=' . $pages . '&sort=' . $sort .'">Next</a>';
     }
 
-    echo '</p>';
+    echo '</div>';
+
 }
+
+echo '</div></div>';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html');
 ?>
