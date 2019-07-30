5cd685d648b44865f44c3f269565d636214a52c2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-07-30 12:51:43 -0700

    Added config, header, and footer files, and name page title

---
+++ register_page2.php
@@ -0,0 +1,11 @@
+<?php
+
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+$page_title = 'Register';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+
+?>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
