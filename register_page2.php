10f4ecf823f1812d1e538a4a995aa94f2b3af4b4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-03 14:47:02 -0700

    Sticky the session inputs along with unsetting them

--- register_page2.php
+++ register_page2.php
@@ -106,30 +106,30 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     </div>
                     <div class="shipping-address-1">
                         <label for="shippingAddress1">Shipping Address</label>
-                        <input type="text" id="shippingAddress1" class="user-address-1--register" name="address1" maxlength="46" value="<?php if (isset($trimmed['address1'])) echo $trimmed['address1']; ?>">
+                        <input type="text" id="shippingAddress1" class="user-address-1--register" name="address1" maxlength="46" value="<?php if (isset($_SESSION['a1'])) echo $_SESSION['a1']; unset($_SESSION['a1']); ?>">
                     </div>
                     <?php if (isset($_SESSION['a1Err'])) echo '<div class="address-1-error--register"><p>' . $_SESSION['a1Err'] . '</p></div>';
                     unset($_SESSION['a1Err']); ?>
                     <div class="shipping-address-2">
                         <label for="shippingAddress2">Apartment / Suite / Other <span class="register-shipping-info">(optional)</span></label>
-                        <input type="text" id="shippingAddress2" class="user-address-2--register" name="address2" maxlength="46" value="<?php if (isset($trimmed['address2'])) echo $trimmed['address2']; ?>">
+                        <input type="text" id="shippingAddress2" class="user-address-2--register" name="address2" maxlength="46" value="<?php if (isset($_SESSION['a2'])) echo $_SESSION['a2']; unset($_SESSION['a2']); ?>">
                     </div>
                     <?php if (isset($_SESSION['a2Err'])) echo '<div class="address-2-error--register"><p>' . $_SESSION['a2Err'] . '</p></div>';
                     unset($_SESSION['a2Err']); ?>
                     <div class="shipping-city">
                         <label for="shippingCity">City</label>
-                        <input type="text" id="shippingCity" class="user-city--register" name="city" maxlength="50" value="<?php if (isset($trimmed['city'])) echo $trimmed['city']; ?>">
+                        <input type="text" id="shippingCity" class="user-city--register" name="city" maxlength="50" value="<?php if (isset($_SESSION['c'])) echo $_SESSION['c']; unset($_SESSION['c']); ?>">
                     </div>
                     <?php if (isset($_SESSION['cErr'])) echo '<div class="city-error--register"><p>' . $_SESSION['cErr'] . '</p></div>';
                     unset($_SESSION['cErr']); ?>
                     <div class="shipping-state-zip">
                         <div class="shipping-state">
                             <label for="shippingState">State</label>
-                            <input type="text" id="shippingState" class="user-state--register" name="state" maxlength="50" placeholder="NJ or New Jersey" value="<?php if (isset($trimmed['state'])) echo $trimmed['state']; ?>">
+                            <input type="text" id="shippingState" class="user-state--register" name="state" maxlength="50" placeholder="NJ or New Jersey" value="<?php if (isset($_SESSION['s'])) echo $_SESSION['s']; unset($_SESSION['s']); ?>">
                         </div>
                         <div class="shipping-zip-code">
                             <label for="shippingZipCode">Zip Code</label>
-                            <input type="text" id="shippingZipCode" class="user-zip--register" name="zip" maxlength="10" placeholder="12345 or 12345-6789" value="<?php if (isset($trimmed['zip'])) echo $trimmed['zip']; ?>">
+                            <input type="text" id="shippingZipCode" class="user-zip--register" name="zip" maxlength="10" placeholder="12345 or 12345-6789" value="<?php if (isset($_SESSION['z'])) echo $_SESSION['z']; unset($_SESSION['z']); ?>">
                         </div>
                     </div>
                     <?php
@@ -143,7 +143,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                     ?>
                     <div class="shipping-phone">
                         <label for="shippingPhone">Phone <span class="register-phone-info">(optional, but may be used to assist delivery)</span></label>
-                        <input type="tel" id="shippingPhone" class="user-phone--register" name="phone_num" placeholder="123-456-7890" maxlength="15" value="<?php if (isset($trimmed['phone_num'])) echo $trimmed['phone_num']; ?>">
+                        <input type="tel" id="shippingPhone" class="user-phone--register" name="phone_num" placeholder="123-456-7890" maxlength="15" value="<?php if (isset($_SESSION['phone_num'])) echo $_SESSION['phone_num']; unset($_SESSION['phone_num']); ?>">
                     </div>
                     <?php if (isset($_SESSION['phoneErr'])) echo '<div class="phone-error--register"><p>' . $_SESSION['phoneErr'] . '</p></div>';
                     unset($_SESSION['phoneErr']); ?>
