1a55d4b593ba3273a3b2b898424caacee1e6e695
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-02 21:55:07 -0700

    Initialized documentation

---
+++ register_complete.php
@@ -0,0 +1,17 @@
+<?php
+
+# register_complete.php (User's Final Registration Page).
+# Created August 2, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
