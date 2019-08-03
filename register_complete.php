41825a2001a3e0a83ada4ef8994ca0d8b7aa9497
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-02 22:37:42 -0700

    Validated address1 and zipcode with error messages

--- register_complete.php
+++ register_complete.php
@@ -43,6 +43,22 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $a1 = $c = $s = $z = FALSE;
 
+    if (preg_match('/^[A-Z0-9 ,#\'\/.]{3,46}$/iu', sanitize_input($_POST['address1']))) {
+        $_SESSION['a1Err'] = '';
+        $_SESSION['a1'] = mysqli_real_escape_string($dbc, sanitize_input($_POST['address1']));
+        $a1 = TRUE;
+    } else {
+        $_SESSION['a1Err'] = 'Please enter a valid shipping address!';
+    }
+
+    if (preg_match('/^(\d{5})(-\d{4})?\s?$/', sanitize_input($_POST['zip']))) {
+        $_SESSION['zipErr'] = '';
+        $_SESSION['z'] = mysqli_real_escape_string($dbc, sanitize_input($_POST['zip']));
+        $z = TRUE;
+    } else {
+        $_SESSION['zipErr'] = 'Please enter a valid zip code!';
+    }
+
 }
 
 ?>
