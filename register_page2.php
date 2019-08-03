4d4f52cd22530ec6befd8f82b5f5f1644176de8b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-02 20:52:07 -0700

    Fixed error message for pass2

--- register_page2.php
+++ register_page2.php
@@ -75,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     } else {
         $_SESSION['passErr1'] = 'Please enter a valid password!';
-        $_SESSION['passErr2'] = '';
+        $_SESSION['passErr2'] = 'Please confirm your new password!';
     }
 
     if ($fn && $ln && $e && $p) {
