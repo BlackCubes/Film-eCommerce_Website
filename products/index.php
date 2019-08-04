5c2bc19daba7ee21a40b56a71bbf5a85efc0145d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-04 14:06:29 -0700

    Created conditional to check if the url meets the requirements, and if not then redirect them to the homepage

--- products/index.php
+++ products/index.php
@@ -28,6 +28,18 @@ function validate_url($url) {
     return filter_var($url, FILTER_VALIDATE_URL) ? true : false;
 }
 
+if (isset($_GET['isd'], $_GET['department'], $_GET['format']) && validate_url('http://localhost/FilmIndustry/eCommerce/products/index.php?isd=' . $_GET['isd'] . '&department=' . $_GET['department'] . '&format=' . $_GET['format']) && ($GET['department'] = ('Movies' || 'TV')) && ($_GET['format'] = ('4K UHD' || 'Blu-ray' || 'DVD'))) {
+
+} else {
+
+    $url = BASE_URL . 'index.php';
+
+    ob_end_clean();
+    header("Location: $url");
+    exit();
+
+}
+
 ?>
 
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
