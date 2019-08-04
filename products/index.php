2355984d0e9f06522e0e26625dbd915e52444306
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-04 12:03:13 -0700

    Added config, header, and footer files, and created a page title variable

--- products/index.php
+++ products/index.php
@@ -14,4 +14,12 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+$page_title = 'Test';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+
 ?>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
