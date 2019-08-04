229de75605f5defae3da0946cc0aecdc7f88245d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-04 11:57:10 -0700

    Created and initialized documentation

---
+++ products/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Display Products Once Clicked).
+# Created on August 4, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
