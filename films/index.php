d8ebd3a12884f403578d63e14e9a9835fa32839b
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-04 22:51:38 -0700

    Fixed the writer query to display the results only if the department is movies

--- films/index.php
+++ films/index.php
@@ -59,7 +59,7 @@ require(MYSQL);
         }
         echo '</div>';
 
-        $q = "SELECT first_name, middle_name, last_name FROM writers ORDER BY last_name";
+        $q = "SELECT DISTINCT w.first_name, w.middle_name, w.last_name FROM writers AS w JOIN products_writers AS pw ON w.id=pw.writer_id JOIN products AS p ON pw.product_id=p.id JOIN departments AS d ON p.department_id=d.id ORDER BY w.last_name";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
         echo '<div class="results"><p><b>Writers</b></p>';
         while ($writer = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
