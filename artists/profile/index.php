beb9886a9e44a5c9a4b0af9f19a547a040a0c071
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-09 20:23:55 -0700

    Created and initialized the documentation

---
+++ artists/profile/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Display Artists once clicked).
+# Created on August 9, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
