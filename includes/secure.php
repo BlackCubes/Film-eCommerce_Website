cebfe9bcffe7e6cdd4fdd3438239f94ee153d39c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-09 20:01:58 -0700

    Created and initialized the documentation

--- includes/secure.php
+++ includes/secure.php
@@ -1,3 +1,17 @@
 <?php
 
+# secure.php (Security page to be used to secure the entire website).
+# Created on August 9, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
 require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
