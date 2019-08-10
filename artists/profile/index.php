72f6e31685e6b55a05912b0cd3881a8d06a3f2f6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-09 22:07:16 -0700

    Debugging

--- artists/profile/index.php
+++ artists/profile/index.php
@@ -26,7 +26,7 @@ if (isset($_GET['artist'], $_GET['role']) /*&& preg_match('/((\bactors\b)|(\bdir
 
     $role = $_GET['role'];
 
-    if (is_numeric($id_decrypt) && preg_match('/^[1-9]{1}([0-9]{1,10})?$/', $id_decrypt)) {
+    if (is_numeric($id_decrypt) /*&& preg_match('/^[1-9]{1}([0-9]{1,10})?$/', $id_decrypt)*/) {
 
         require(MYSQL);
 
