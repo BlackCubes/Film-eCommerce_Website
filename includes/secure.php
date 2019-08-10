59f68499cf9768ae2d5836f4397d89cf61aa9e45
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-09 20:12:50 -0700

    Created two functions that encrypts and decrypts data by using openssl

--- includes/secure.php
+++ includes/secure.php
@@ -15,3 +15,24 @@
 // *************************************** //
 
 require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+// ********************************************** //
+// ************ ENCRYPT/DECRYPT DATA ************ //
+
+// Encrypt the data
+function my_encrypt($data, $key) {
+    $encryption_key = base64_decode($key);
+    $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+    $encrypted = openssl_encrypt($data, 'aes-256-cbc', $encryption_key, 0, $iv);
+    return base64_encode($encrypted . '::' . $iv);
+}
+
+// Decrypt the data
+function my_decrypt($data, $key) {
+    $encryption_key = base64_decode($key);
+    list($encrypted_data, $iv) = explode('::', base64_decode($data), 2);
+    return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
+}
+
+// ************ ENCRYPT/DECRYPT DATA ************ //
+// ********************************************** //
