3b6f1fd063088170cb19753b1a9070cd79908ac2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-11 11:15:56 -0700

    Created and initialized documentation

---
+++ studios/profile/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Display Studios once clicked).
+# Created on August 11, 2019.
+# Created by Elias Gutierrez
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
