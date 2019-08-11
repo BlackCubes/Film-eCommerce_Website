f315090ee26c2144c9477e731b2f484bc92937e4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-11 11:26:32 -0700

    Created second if-conditional that checks if the decrypted id is numeric and also using regex, and if not then redirect the user to homepage

--- studios/profile/index.php
+++ studios/profile/index.php
@@ -21,6 +21,18 @@ require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/secure.php')
 if (isset($_GET['role'], $_GET['company']) && preg_match('/(\bstudio\b)(?!;)/', $_GET['role'])) {
 
     $id_decrypt = urldecode(my_decrypt($_GET['company'], KEY));
+
+    if (is_numeric($id_decrypt) && preg_match('/^[1-9]{1}([0-9]{1,10})?$/', $id_decrypt)) {
+
+    } else {
+
+        $url = BASE_URL . 'index.php';
+
+        ob_end_clean();
+        header("Location: $url");
+        exit();
+
+    }
     
 } else {
 
