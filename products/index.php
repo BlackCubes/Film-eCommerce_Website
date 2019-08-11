ab5df3b924f68628328f91c751d47810c20b4f04
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-10 23:27:08 -0700

    Encrypted the writer id for url

--- products/index.php
+++ products/index.php
@@ -81,7 +81,11 @@ if (isset($_GET['isd']) /*&& validate_url('http://localhost/FilmIndustry/eCommer
         echo '</tbody></table></div><div class="writer-list-container--product-view"><div class="writer-title--product-view"><h4>Writers</h4></div><table class="writer-list--product-view"><thead><tr><th colspan="2" class="space--product-view"></th></tr></thead><tbody>';
 
         while ($writer = mysqli_fetch_array($r_writer, MYSQLI_ASSOC)) {
+
+            $writer_id_encrypt = urlencode(my_encrypt($writer['writer_id'], KEY));
+
             echo '<tr><td class="cast-crew-image--product-view"><a href="#"><img alt="' . $writer['writer_fn'] . ' ' . $writer['writer_mn'] . ' ' . $writer['writer_ln'] . '" title="' . $writer['writer_fn'] . ' ' . $writer['writer_mn'] . ' ' . $writer['writer_ln'] . '" src="/FilmIndustry/eCommerce/img/' . $writer['writer_img'] . '"></a></td><td class="cast-crew-name--product-view"><span class="cast-crew-dot-dots"> . . . . </span><a href="#">' . $writer['writer_fn'] . ' ' . $writer['writer_mn'] . ' ' . $writer['writer_ln'] . '</a></td></tr>';
+
         }
 
         echo '</tbody></table></div><div class="actor-list-container--product-view"><div class="actor-title--product-view"><h4>Actors</h4></div><table class="actor-list--product-view"><thead><tr><th colspan="2" class="space--product-view"></th></tr></thead><tbody>';
