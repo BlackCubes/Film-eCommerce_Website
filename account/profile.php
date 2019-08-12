16f86f80d8cf5a6947890463739f1f94da8e181d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-11 18:44:56 -0700

    Added a tags in all inner containers for profile-option

--- account/profile.php
+++ account/profile.php
@@ -36,52 +36,64 @@ if (!isset($_SESSION['id'])) {
     </div>
     <div class="profile-option--profile-view">
         <div class="container-login-security--profile-view">
-            <div class="login-security-title--profile-view">
-                <h4>Login & security</h4>
-            </div>
-            <div class="login-security-info--profile-view">
-                <p>Edit login, name, and mobile number, and delete account</p>
-            </div>
+            <a href="#">
+                <div class="login-security-title--profile-view">
+                    <h4>Login & security</h4>
+                </div>
+                <div class="login-security-info--profile-view">
+                    <p>Edit login, name, and mobile number, and delete account</p>
+                </div>
+            </a>
         </div>
         <div class="container-upload-image--profile-view">
-            <div class="upload-image-title--profile-view">
-                <h4>Upload your image</h4>
-            </div>
-            <div class="upload-image-info--profile-view">
-                <p>Upload your profile picture</p>
-            </div>
+            <a href="#">
+                <div class="upload-image-title--profile-view">
+                    <h4>Upload your image</h4>
+                </div>
+                <div class="upload-image-info--profile-view">
+                    <p>Upload your profile picture</p>
+                </div>
+            </a>
         </div>
         <div class="container-address--profile-view">
-            <div class="address-title--profile-view">
-                <h4>Your address</h4>
-            </div>
-            <div class="address-info--profile-view">
-                <p>Edit address for orders</p>
-            </div>
+            <a href="#">
+                <div class="address-title--profile-view">
+                    <h4>Your address</h4>
+                </div>
+                <div class="address-info--profile-view">
+                    <p>Edit address for orders</p>
+                </div>
+            </a>
         </div>
         <div class="container-orders--profile-view">
-            <div class="orders-title--product-view">
-                <h4>Your Orders</h4>
-            </div>
-            <div class="orders-info--product-view">
-                <p>Track, return, or buy things again</p>
-            </div>
+            <a href="#">
+                <div class="orders-title--product-view">
+                    <h4>Your Orders</h4>
+                </div>
+                <div class="orders-info--product-view">
+                    <p>Track, return, or buy things again</p>
+                </div>
+            </a>
         </div>
         <div class="container-wishlist--profile-view">
-            <div class="wishlist-title--product-view">
-                <h4>Your Wishlists</h4>
-            </div>
-            <div class="wishlist-info--product-view">
-                <p>View, edit, or delete your wishlists</p>
-            </div>
+            <a href="#">
+                <div class="wishlist-title--product-view">
+                    <h4>Your Wishlists</h4>
+                </div>
+                <div class="wishlist-info--product-view">
+                    <p>View, edit, or delete your wishlists</p>
+                </div>
+            </a>
         </div>
         <div class="container-payment--profile-view">
-            <div class="payment-title--profile-view">
-                <h4>Payment options</h4>
-            </div>
-            <div class="payment-info--profile-view">
-                <p>Edit or add payment methods</p>
-            </div>
+            <a href="#">
+                <div class="payment-title--profile-view">
+                    <h4>Payment options</h4>
+                </div>
+                <div class="payment-info--profile-view">
+                    <p>Edit or add payment methods</p>
+                </div>
+            </a>
         </div>
     </div>
 </div>
