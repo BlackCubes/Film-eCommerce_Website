23d90f75036b867d49b5198d1d327697976bea4e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-14 05:53:30 -0700

    Created and initialized the documentation

---
+++ cart/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Code to handle the Shopping Cart).
+# Created on August 14, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
