631c80130425a40cb65a49606e1191e5b7673364
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-16 06:19:30 -0700

    Created a foreach loop that iterates through the session array and deletes the product session or the entire session

--- cart/index.php
+++ cart/index.php
@@ -75,7 +75,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 case "remove":
 
                     if (!empty($_SESSION['cart_item'])) {
-                        
+                        foreach ($_SESSION['cart_item'] as $k => $v) {
+                            if ($product_isd == $k) unset($_SESSION['cart_item'][$k]);
+                            if (empty($_SESSION['cart_item'])) unset($_SESSION['cart_item']);
+                        }
                     }
 
                     break;
