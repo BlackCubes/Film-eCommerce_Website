229720c7bce625d9a6ffc9b2fcc5b3e66c09dc2d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-19 06:35:37 -0700

    Checked the see if the user is logged in or if the supplier is, and if user is not logged in or supplier is logged in then redirect

--- cart/cart.php
+++ cart/cart.php
@@ -20,7 +20,13 @@ require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/secure.php')
 
 $page_title = 'Shopping Cart';
 
-include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html')
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+
+if (!isset($_SESSION['id']) || isset($_SESSION['supplier_id'])) {
+    ob_end_clean();
+    header("Location: http://localhost/FilmIndustry/eCommerce/index.php");
+    exit();
+}
 
 ?>
 
