a36c0cf24dbd4521f218da9cfd57f2c0ed8cf60c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-19 06:28:52 -0700

    Created shopping cart view page

---
+++ cart/cart.php
@@ -0,0 +1,17 @@
+<?php
+
+# cart.php (Users view Shopping Cart before checkout).
+# Created on August 18, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
