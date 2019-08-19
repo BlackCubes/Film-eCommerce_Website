6ce5a8a44b67ca151926e52aed2434bd834ce627
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-19 06:32:45 -0700

    Added the config, secure, header and footer files, and created page title

--- cart/cart.php
+++ cart/cart.php
@@ -14,4 +14,14 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/config.inc.php');
+
+require($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/secure.php');
+
+$page_title = 'Shopping Cart';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html')
+
 ?>
+
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
