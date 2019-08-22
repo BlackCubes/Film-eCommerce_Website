dd66ac9a9d6f2564273ba5073fca03c22a094a1e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-21 20:47:22 -0700

    Initialized first if-conditional with DB connection inside

--- account/change_name.php
+++ account/change_name.php
@@ -29,6 +29,12 @@ if (!isset($_SESSION['id']) || isset($_SESSION['supplier_id'])) {
     exit();
 }
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    
+    require(MYSQL);
+
+}
+
 ?>
 <form action="change_name.php" method="post" id="userChangeName">
     <fieldset class="change-name-card">
