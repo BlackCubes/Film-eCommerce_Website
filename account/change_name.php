3d7da2d44bd7f4a71939859879e783ca92a4245d
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-21 20:43:16 -0700

    Finished the majority of the html form fields

--- account/change_name.php
+++ account/change_name.php
@@ -32,7 +32,29 @@ if (!isset($_SESSION['id']) || isset($_SESSION['supplier_id'])) {
 ?>
 <form action="change_name.php" method="post" id="userChangeName">
     <fieldset class="change-name-card">
-        
+        <div class="change-name-title">
+            <h1>Change your name</h1>
+        </div>
+        <div class="change-name-info">
+            <p>If you want to change your name associated with your account, you may do so below. Be sure to click the <b>Save Changes</b> button when you are done.</p>
+        </div>
+        <div class="change-full-name">
+            <div class="first-name--change-name">
+                <label for="changeFirstName">New first name</label>
+                <input type="text" id="changeFirstName" class="user-first-name--change-name" name="first_name" maxlength="20" value="<?php if (isset($_POST['first_name'])) echo sanitize_input($_POST['first_name']); ?>">
+            </div>
+            <div class="middle-name--change-name">
+                <label for="changeMiddleName">New middle name (optional)</label>
+                <input type="text" id="changeMiddleName" class="user-middle-name--change-name" name="middle_name" maxlength="40" value="<?php if (isset($_POST['middle_name'])) echo sanitize_input($_POST['middle_name']); ?>">
+            </div>
+            <div class="last-name--change-name">
+                <label for="changeLastName">New last name</label>
+                <input type="text" id="changeLastName" class="user-last-name--change-name" name="last_name" maxlength="40" value="<?php if (isset($_POST['last_name'])) echo sanitize_input($_POST['last_name']); ?>">
+            </div>
+        </div>
+        <div class="change-name-submit">
+            <input type="submit" class="change-name-button" name="submit" value="Save Changes">
+        </div>
     </fieldset>
 </form>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
