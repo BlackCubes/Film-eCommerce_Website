db31644e7acbfe374fb288d71aa85a77c1c4573c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-21 19:58:24 -0700

    Created and initialized documentation

---
+++ account/change_name.php
@@ -0,0 +1,17 @@
+<?php
+
+# change_name.php (User Changes Name).
+# Created on August 21, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
