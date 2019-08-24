e6a9dfee74679b5c75ca570586378ab90d0efd01
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-24 10:22:10 -0700

    Created and initialized documentation

---
+++ account/change_email.php
@@ -0,0 +1,17 @@
+<?php
+
+# change_email.php (User Changes Email).
+# Created on August 24, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
