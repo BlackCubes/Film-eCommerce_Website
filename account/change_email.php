f7c7a5c1f39acb07c62a3290b4897065abc1e486
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-24 10:57:54 -0700

    Finished the main HTML form fields

--- account/change_email.php
+++ account/change_email.php
@@ -35,6 +35,25 @@ if (!isset($_SESSION['id']) || isset($_SESSION['supplier_id'])) {
         <div class="change-email-title">
             <h2>Change email</h2>
         </div>
+        <div class="current-email--change-email">
+            <label for="currentEmail">Old email address</label>
+            <div id="currentEmail" class="user-current-email--change-email"></div>
+        </div>
+        <div class="new-email-input--change-email">
+            <label for="changeNewEmail">New email address</label>
+            <input type="email" id="changeNewEmail" class="user-new-email--change-email" name="email_1" maxlength="80" value="<?php if (isset($_POST['email_1'])) echo sanitize_input($_POST['email']); ?>">
+        </div>
+        <div class="confirm-email-input--change-email">
+            <label for="changeConfirmEmail">Re-enter new email</label>
+            <input type="email" id="changeConfirmEmail" class="user-confirm-email--change-email" name="email_2" maxlength="80" value="<?php if (isset($_POST['email_2'])) echo sanitize_input($_POST['email_2']); ?>">
+        </div>
+        <div class="confirm-password-input--change-email">
+            <label for="changeConfirmPass">Password</label>
+            <input type="password" id="changeConfirmPass" class="user-confirm-password--change-email" name="password" maxlength="255">
+        </div>
+        <div class="change-email-submit">
+            <input type="submit" class="change-email-button" name="submit" value="SAVE CHANGES">
+        </div>
     </fieldset>
 </form>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
