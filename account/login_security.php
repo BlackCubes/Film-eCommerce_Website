2f2e2c6517f4f5aa58a96a6eefd4b6da940151c6
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-25 13:41:07 -0700

    Created and initialized documentation

---
+++ account/login_security.php
@@ -0,0 +1,17 @@
+<?php
+
+# login_security.php (User Selects Login and Security Options).
+# Created on August 25, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
