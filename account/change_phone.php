88ae3e2a463d3cd5159bf4a033d079b996b91675
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-24 21:28:51 -0700

    Did some of the form fields

--- account/change_phone.php
+++ account/change_phone.php
@@ -23,4 +23,17 @@ $page_title = 'Change Phone Number';
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 
 ?>
+<form action="change_phone.php" method="post" id="userChangePhoneCard">
+    <fieldset class="change-phone-card">
+        <div class="change-phone-title">
+            <h2>Change Mobile Phone Number</h2>
+        </div>
+        <div class="current-phone--change-phone">
+            <label for="currentPhone">Old mobile phone number</label>
+            <div id="currentPhone" class="user-current-phone--change-phone">
+                <p><?php if (isset($_SESSION['phone_num'])) {echo $_SESSION['phone_num'];} else {echo 'None';} ?></p>
+            </div>
+        </div>
+    </fieldset>
+</form>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
