383e4e22c4bfbe9b9db6efc64daaa979d331e798
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-24 21:13:49 -0700

    Created and initialized documentation

---
+++ account/change_phone.php
@@ -0,0 +1,17 @@
+<?php
+
+# change_phone.php (User Changes Phone Number).
+# Created on August 24, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
