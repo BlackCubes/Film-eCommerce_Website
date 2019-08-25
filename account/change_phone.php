af565992594dff170879ab8cefb13708c7f3b520
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-25 10:27:13 -0700

    Added update query and stored it in result variable

--- account/change_phone.php
+++ account/change_phone.php
@@ -70,6 +70,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
                 mysqli_autocommit($dbc, FALSE);
 
+                $q = "UPDATE users SET phone_num='" . $pn . "' WHERE id={$_SESSION['id']} LIMIT 1";
+
+                $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
             } else {
                 $systemErr = 'Please try again';
             }
