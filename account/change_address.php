f032d6948cc978f9602a229f4284253ef1d68355
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-25 20:07:25 -0700

    Created and initialized the documentation

---
+++ account/change_address.php
@@ -0,0 +1,17 @@
+<?php
+
+# change_address.php (User Changes Address).
+# Created on August 25, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
