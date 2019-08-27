7c8ce57c79951d4c83b3d83e8f48674b14fd8413
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-27 06:42:46 -0700

    Created and initialized the documentation

---
+++ wishlist/index.php
@@ -0,0 +1,17 @@
+<?php
+
+# index.php (Code to handle the Wishlists).
+# Created on August 27, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
