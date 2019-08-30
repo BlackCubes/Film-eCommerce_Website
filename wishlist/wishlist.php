30904f80ecab14e601b3225921a9c136db59d8f4
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-29 21:45:04 -0700

    Created first if-conditional with else statement

--- wishlist/wishlist.php
+++ wishlist/wishlist.php
@@ -32,5 +32,13 @@ require(MYSQL);
 
 $q = "SELECT product_department FROM wishlists WHERE user_id={$_SESSION['id']}";
 $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
+if (mysqli_num_rows($r) == 1){
 ?>
-<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
+<?php } else { ?>
+<p>Your wishlist is empty!</p>
+<?php
+}
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html');
+
+?>
