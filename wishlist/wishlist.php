7b2779b530c831b48f7616ddb91369a8aae25537
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-30 18:25:05 -0700

    Created query to display the wishlists

--- wishlist/wishlist.php
+++ wishlist/wishlist.php
@@ -34,6 +34,11 @@ $q = "SELECT product_department FROM wishlists WHERE user_id={$_SESSION['id']} L
 $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
 if (mysqli_num_rows($r) == 1){
+
+    $q = "SELECT DISTINCT p.name AS product_name, p.unit_price AS product_price, p.stock AS product_stock, p.image_1 AS product_image, a.first_name AS actor_fn, a.middle_name AS actor_mn, a.last_name AS actor_ln, wl.product_department AS product_department, wl.product_format AS product_format, wl.quantity AS product_quantity, wl.date_created AS product_date_created FROM wishlists AS wl JOIN products AS p ON wl.product_id=p.id JOIN products_actors AS pa ON p.id=pa.product_id JOIN actors AS a ON pa.actor_id=a.id WHERE wl.user_id={$_SESSION['id']} ORDER BY a.last_name LIMIT 2";
+
+    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
 ?>
 <div class="container--wishlist-view">
     <h2>Your Wishlists</h2>
