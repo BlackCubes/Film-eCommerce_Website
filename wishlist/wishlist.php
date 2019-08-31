611bcd2c90d911ede22925c5fea7f1af6b244c10
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-31 08:11:29 -0700

    Changed foreach loop to while loop

--- wishlist/wishlist.php
+++ wishlist/wishlist.php
@@ -46,6 +46,7 @@ if (mysqli_num_rows($r) == 1){
             <h2>Your Wishlists</h2>
         </div>
         <div class="results--wishlist-view">
+            <?php while ($wishlist = mysqli_fetch_array($r, MYSQLI_ASSOC)) { ?>
             <div class="wish-list--wishlist-view">
                 <div class="product-image--wishlist-view">
                     <a href="/FilmIndustry/eCommerce/products/index.php?isd=<?php echo $wishlist['product_isd']; ?>"><img alt="<?php echo $wishlist['product_name']; ?>" src="/FilmIndustry/uploads/products/<?php echo $wishlist['product_image']; ?>"></a>
@@ -82,6 +83,7 @@ if (mysqli_num_rows($r) == 1){
                     </div>
                 </div>
             </div>
+            <?php } ?>
         </div>
     </div>
 </div>
