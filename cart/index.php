a7d027951d652d84b8206894084e15f5db600bbb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-31 22:37:02 -0700

    Added foreach loop to unset a specific product in session

--- cart/index.php
+++ cart/index.php
@@ -159,7 +159,10 @@ if (preg_match('/((\badd\b)|(\bdelete\b)|(\blater\b))(?!;)?/', $_GET['action']))
 
                         mysqli_commit($dbc);
 
-                        unset($_SESSION['cart_item']);
+                        foreach ($_SESSION['cart_item'] as $k => $v) {
+                            if ($product_isd == $k) unset($_SESSION['cart_item'][$k]);
+                            if (empty($_SESSION['cart_item'])) unset($_SESSION['cart_item']);
+                        }
 
                         $url = BASE_URL . 'cart/cart.php';
                         mysqli_close($dbc);
