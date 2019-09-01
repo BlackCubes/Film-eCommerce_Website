2514ae4578cd53396483f542de343fc596918f5e
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-31 20:03:37 -0700

    Created an update query if the product does exist in wishlist table DB

--- wishlist/index.php
+++ wishlist/index.php
@@ -89,6 +89,10 @@ if (preg_match('/((\badd\b)|(\bmove\b)|(\bdelete\b)|(\bcart\b))(?!;)?/', $_GET['
 
                 } elseif (mysqli_num_rows($r) == 1) {
 
+                    $q = "UPDATE wishlists SET quantity=quantity+1 WHERE product_id={$product_id} AND user_id={$_SESSION['id']}";
+
+                    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
                 }
 
                 break;
