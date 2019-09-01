a6aed727f566807f16cb24ab4cce95240352bdda
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-31 19:24:32 -0700

    Deleted self-comment

--- wishlist/index.php
+++ wishlist/index.php
@@ -162,8 +162,6 @@ if (preg_match('/((\badd\b)|(\bmove\b)|(\bdelete\b)|(\bcart\b))(?!;)?/', $_GET['
 
             case "cart":
 
-                /* CHECK FOR CASE OF QUANTITY!!!!! */
-
                 $q = "SELECT wl.product_id AS product_id, p.isd AS product_isd, p.name AS product_name, p.unit_price AS product_price, p.stock AS product_stock, p.image_1 AS product_image, wl.product_department AS product_department, wl.product_format AS product_format, dir.first_name AS director_fn, dir.middle_name AS director_mn, dir.last_name AS director_ln, wl.quantity AS quantity FROM wishlists AS wl JOIN products AS p ON wl.product_id=p.id JOIN products_directors AS pdir ON p.id=pdir.product_id JOIN directors AS dir on pdir.director_id=dir.id WHERE wl.user_id={$_SESSION['id']} AND p.isd='" . $product_isd . "'";
 
                 $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
