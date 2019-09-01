2e7228865ca1f1a0f28828e3caba5769f3d88134
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-08-31 19:06:01 -0700

    Fixed variable name for multi array

--- wishlist/index.php
+++ wishlist/index.php
@@ -170,7 +170,7 @@ if (preg_match('/((\badd\b)|(\bmove\b)|(\bdelete\b)|(\bcart\b))(?!;)?/', $_GET['
 
                 $single_product = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
-                $item_carts = array($single_product['product_isd'] => array("product_id" => $single_product['product_id'], "product_isd" => $single_product['product_isd'], "product_name" => $single_product['product_name'], "product_price" => $single_product['product_price'], "product_stock" => $single_product['product_stock'], "product_image" => $single_product['product_image'], "product_department" => $single_product['product_department'], "product_format" => $single_product['product_format'], "director_fn" => $single_product['director_fn'], "director_mn" => $single_product['director_mn'], "director_ln" => $single_product['director_ln'], "quantity" => $single_product['quantity']));
+                $items_cart = array($single_product['product_isd'] => array("product_id" => $single_product['product_id'], "product_isd" => $single_product['product_isd'], "product_name" => $single_product['product_name'], "product_price" => $single_product['product_price'], "product_stock" => $single_product['product_stock'], "product_image" => $single_product['product_image'], "product_department" => $single_product['product_department'], "product_format" => $single_product['product_format'], "director_fn" => $single_product['director_fn'], "director_mn" => $single_product['director_mn'], "director_ln" => $single_product['director_ln'], "quantity" => $single_product['quantity']));
 
                 if (!empty($_SESSION['cart_item'])) {
                     if (in_array($single_product['product_isd'], array_keys($_SESSION['cart_item']))) {
