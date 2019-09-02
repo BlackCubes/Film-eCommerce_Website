2a3417a05bf9b172e931e330472df6820c1aa288
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-02 13:11:12 -0700

    Added an extension for validation in first if-conditional

--- products/other.php
+++ products/other.php
@@ -22,7 +22,7 @@ $page_title = 'See All Results';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 
-if (isset($_GET['type'], $_GET['name'])) {
+if (isset($_GET['department'], $_GET['format'], $_GET['type']) && preg_match('/((\bMovies\b)|(\bTV\b))(?!;)/', $_GET['department']) /*&& preg_match('/((\b\b))(?!;)/', $_GET['format'])*/ && preg_match('/((\bgenres\b)|(\bactors\b)|(\bdirectors\b)|(\bwriters\b)|(\bproducers\b)|(\bdps\b)|(\bstudios\b))(?!;)/', $_GET['type'])) {
 
     require(MYSQL);
 ?>
