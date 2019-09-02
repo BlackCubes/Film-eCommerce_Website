b8bd4c14f3ea3c37e0728e56226af7bd00f9e6c3
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-02 13:57:25 -0700

    Added query for case of genres

--- products/other.php
+++ products/other.php
@@ -36,6 +36,10 @@ if (isset($_GET['department'], $_GET['format'], $_GET['type']) && preg_match('/(
 
         case "genres":
 
+            $q = "SELECT DISTINCT g.genre AS genres FROM genres g JOIN products_genres pg ON g.id=pg.genre_id JOIN products p ON pg.product_id=p.id JOIN departments d ON p.department_id=d.id WHERE d.department='" . $department . "'";
+
+            $r_genre = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+
             break;
         
         case "actors":
