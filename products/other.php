c4778b7c30557db0e012811d5bef4b5fdbef2b34
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-02 12:18:00 -0700

    Created and initialized documentation

---
+++ products/other.php
@@ -0,0 +1,17 @@
+<?php
+
+# other.php (Users pick the extra options in sidebar to view for products).
+# Created on September 2, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
