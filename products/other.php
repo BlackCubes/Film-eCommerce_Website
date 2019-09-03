7dc2c24c0881e90c3ffa9c28fb33e5b3be1c0f20
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-02 17:03:04 -0700

    Fixing the new way

--- products/other.php
+++ products/other.php
@@ -103,15 +103,21 @@ if (isset($_GET['department'], $_GET['format'], $_GET['type']) && preg_match('/(
         while ($genre = mysqli_fetch_array($r_genre, MYSQLI_ASSOC)) {
 ?>
 <?php
+            echo '<ul>';
             for ($i = 0; $i < $row_display; $i++) {
-                echo '<ul><li><a href="#>' . $genre['genres'] . '</a></li></ul>';
+                echo '<li><a href="#>' . $genre['genres'] . '</a></li>';
             }
+            echo '</ul>';
+            echo '<ul>';
             for ($i = 0; $i < $row_display; $i++) {
-                echo '<ul><li><a href="#>' . $genre['genres'] . '</a></li></ul>';
+                echo '<li><a href="#>' . $genre['genres'] . '</a></li>';
             }
+            echo '</ul>';
+            echo '<ul>';
             for ($i = 0; $i < $row_display; $i++) {
-                echo '<ul><li><a href="#>' . $genre['genres'] . '</a></li></ul>';
+                echo '<li><a href="#>' . $genre['genres'] . '</a></li>';
             }
+            echo '</ul>';
 ?>
 <?php
         }
