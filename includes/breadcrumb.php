c6dd3acaa3a4db039db1f2bde8604cef4ab20506
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-06 12:46:17 -0700

    Created and initialized the documentation

---
+++ includes/breadcrumb.php
@@ -0,0 +1,17 @@
+<?php
+
+# breadcrumb.php (File to handle the Breadcrumbs).
+# Created on September 6, 2019.
+# Created by Elias Gutierrez.
+
+// *************************************** //
+// ************ DOCUMENTATION ************ //
+
+/*
+
+*/
+
+// ************ DOCUMENTATION ************ //
+// *************************************** //
+
+?>
