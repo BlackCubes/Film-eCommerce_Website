2e6e8ce562caa83d72fc7649d01af9df8bcc3691
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-07 14:09:37 -0700

    Added breadcrumb file

--- dp/index.php
+++ dp/index.php
@@ -23,6 +23,8 @@ $page_title = 'Cinematographers';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/breadcrumb.php');
+
 require(MYSQL);
 
 $display = 21;
