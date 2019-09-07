3d0c25f134917f565a276dbe9db24b84a9a0d716
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-07 14:45:42 -0700

    Added breadcrumb file

--- products/other.php
+++ products/other.php
@@ -22,6 +22,8 @@ $page_title = 'See All Results';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/breadcrumb.php');
+
 if (isset($_GET['department'], $_GET['format'], $_GET['type']) && preg_match('/((\bMovies\b)|(\bTV\b))(?!;)/', $_GET['department']) /*&& preg_match('/((\b\b))(?!;)/', $_GET['format'])*/ && preg_match('/((\bgenre\b)|(\bactor\b)|(\bdirector\b)|(\bwriter\b)|(\bproducer\b)|(\bdp\b)|(\bstudio\b))(?!;)/', $_GET['type'])) {
 
     require(MYSQL);
