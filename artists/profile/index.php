faeed1977c511fd98dac484a8d031362dbc32da0
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-07 13:54:35 -0700

    Added breadcrumb file

--- artists/profile/index.php
+++ artists/profile/index.php
@@ -48,6 +48,8 @@ if (isset($_GET['artist'], $_GET['role']) && preg_match('/((\bactor\b)|(\bdirect
 
             include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 
+            include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/breadcrumb.php');
+
             echo '<div class="container--artist-view"><div class="main-info--artist-view"><div class="artist-name--artist-view"><h2>' . $artist['artist_fn'] . ' ' . $artist['artist_mn'] . ' ' . $artist['artist_ln'] . '</h2></div><div class="image--artist-view"><img src="/FilmIndustry/eCommerce/img/' . $artist['artist_img'] . '"></div></div><div class="product-artist-info--artist-view"><div class="product-artist-title--artist-view"><h2>Filmography</h2></div><div class="product-artist-list--artist-view">';
 
             while ($product_artist = mysqli_fetch_array($r_pa, MYSQLI_ASSOC)) {
