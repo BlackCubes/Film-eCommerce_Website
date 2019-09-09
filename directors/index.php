18055f525f7e156680101818e07227d6a80f7dbb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-09 07:03:59 -0700

    Added breadcrumb in container and div element

--- directors/index.php
+++ directors/index.php
@@ -66,9 +66,8 @@ if (isset($_GET['s']) && is_numeric($_GET['s'])) {
 
 $breadcrumb = new breadcrumb();
 
-echo $breadcrumb->build(array('Directors' => 'directors/index.php'));
-
 echo '<div class="container" id="templateArtists">
+<div class="breadcrumb">' . $breadcrumb->build(array('Directors' => 'directors/index.php')) . '</div>
 <div class="sidebar-a">
     <div class="results">
         <p><b>Gender</b></p>
