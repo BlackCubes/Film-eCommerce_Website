ef7965e5d7eaf447633f41ef36a6464e2af0a2ae
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-13 22:22:51 -0700

    Stored the Session quantity into queries

--- wishlist/index.php
+++ wishlist/index.php
@@ -64,7 +64,7 @@ if (preg_match('/((\badd\b)|(\bmove\b)|(\bdelete\b)|(\bcart\b))(?!;)?/', $_GET['
 
                 if (mysqli_num_rows($r) == 0) {
 
-                    $q = "INSERT INTO wishlists (product_id, product_department, product_format, quantity, date_created, date_modified, user_id) VALUES ({$product_id[0]}, '" . $product_department[0] . "', '" . $product_format[0] . "', 1, NOW(), NOW(), {$_SESSION['id']})";
+                    $q = "INSERT INTO wishlists (product_id, product_department, product_format, quantity, date_created, date_modified, user_id) VALUES ({$product_id[0]}, '" . $product_department[0] . "', '" . $product_format[0] . "', {$_SESSION['quantity_cart']}, NOW(), NOW(), {$_SESSION['id']})";
 
                     $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
@@ -89,7 +89,7 @@ if (preg_match('/((\badd\b)|(\bmove\b)|(\bdelete\b)|(\bcart\b))(?!;)?/', $_GET['
 
                 } elseif (mysqli_num_rows($r) == 1) {
 
-                    $q = "UPDATE wishlists SET quantity=quantity+1, date_modified=NOW() WHERE product_id={$product_id[0]} AND user_id={$_SESSION['id']}";
+                    $q = "UPDATE wishlists SET quantity=quantity+{$_SESSION['quantity_cart']}, date_modified=NOW() WHERE product_id={$product_id[0]} AND user_id={$_SESSION['id']}";
 
                     $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
