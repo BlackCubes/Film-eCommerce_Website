7a4b1b06cf5a51a7b71ce5d62cbf8b317f885f9a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-13 21:28:04 -0700

    Removed SERVER REQUEST METHOD if-conditional in add case

--- cart/index.php
+++ cart/index.php
@@ -48,49 +48,35 @@ if (preg_match('/((\badd\b)|(\bdelete\b)|(\blater\b)|(\bomit\b)|(\bmove\b))(?!;)
 
             case "add":
 
-                if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $q = "SELECT p.id AS product_id, p.isd AS product_isd, p.name AS product_name, p.unit_price AS product_price, p.stock AS product_stock, p.image_1 AS product_image, d.department AS product_department, f.format AS product_format, dir.first_name AS director_fn, dir.middle_name AS director_mn, dir.last_name AS director_ln FROM products AS p JOIN departments AS d ON p.department_id=d.id JOIN formats AS f ON p.format_id=f.id JOIN products_directors AS pdir ON p.id=pdir.product_id JOIN directors AS dir ON pdir.director_id=dir.id WHERE p.isd='" . $product_isd . "'";
 
-                    $q = "SELECT p.id AS product_id, p.isd AS product_isd, p.name AS product_name, p.unit_price AS product_price, p.stock AS product_stock, p.image_1 AS product_image, d.department AS product_department, f.format AS product_format, dir.first_name AS director_fn, dir.middle_name AS director_mn, dir.last_name AS director_ln FROM products AS p JOIN departments AS d ON p.department_id=d.id JOIN formats AS f ON p.format_id=f.id JOIN products_directors AS pdir ON p.id=pdir.product_id JOIN directors AS dir ON pdir.director_id=dir.id WHERE p.isd='" . $product_isd . "'";
+                $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
-                    $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+                $single_product = mysqli_fetch_array($r, MYSQLI_ASSOC);
 
-                    $single_product = mysqli_fetch_array($r, MYSQLI_ASSOC);
+                $items_cart = array($single_product['product_isd'] => array("product_id" => $single_product['product_id'], "product_isd" => $single_product['product_isd'], "product_name" => $single_product['product_name'], "product_price" => $single_product['product_price'], "product_stock" => $single_product['product_stock'], "product_image" => $single_product['product_image'], "product_department" => $single_product['product_department'], "product_format" => $single_product['product_format'], "director_fn" => $single_product['director_fn'], "director_mn" => $single_product['director_mn'], "director_ln" => $single_product['director_ln'], "quantity" => $_POST['quantity']));
 
-                    $items_cart = array($single_product['product_isd'] => array("product_id" => $single_product['product_id'], "product_isd" => $single_product['product_isd'], "product_name" => $single_product['product_name'], "product_price" => $single_product['product_price'], "product_stock" => $single_product['product_stock'], "product_image" => $single_product['product_image'], "product_department" => $single_product['product_department'], "product_format" => $single_product['product_format'], "director_fn" => $single_product['director_fn'], "director_mn" => $single_product['director_mn'], "director_ln" => $single_product['director_ln'], "quantity" => $_POST['quantity']));
-
-                    if (!empty($_SESSION['cart_item'])) {
-                        if (in_array($single_product['product_isd'], array_keys($_SESSION['cart_item']))) {
-                            foreach ($_SESSION['cart_item'] as $k => $v) {
-                                if ($single_product['product_isd'] == $k) {
-                                    if (empty($_SESSION['cart_item'][$k]['quantity'])) {
-                                        $_SESSION['cart_item'][$k]['quantity'] = 0;
-                                    }
-                                    $_SESSION['cart_item'][$k]['quantity'] += $_POST['quantity'];
+                if (!empty($_SESSION['cart_item'])) {
+                    if (in_array($single_product['product_isd'], array_keys($_SESSION['cart_item']))) {
+                        foreach ($_SESSION['cart_item'] as $k => $v) {
+                            if ($single_product['product_isd'] == $k) {
+                                if (empty($_SESSION['cart_item'][$k]['quantity'])) {
+                                    $_SESSION['cart_item'][$k]['quantity'] = 0;
                                 }
+                                $_SESSION['cart_item'][$k]['quantity'] += $_POST['quantity'];
                             }
-
-                            $url = BASE_URL . 'cart/cart.php';
-                            mysqli_free_result($r);
-                            mysqli_close($dbc);
-                            ob_end_clean();
-                            header("Location: $url");
-                            exit();
-
-                        } else {
-
-                            $_SESSION['cart_item'] = array_merge($_SESSION['cart_item'], $items_cart);
-
-                            $url = BASE_URL . 'cart/cart.php';
-                            mysqli_free_result($r);
-                            mysqli_close($dbc);
-                            ob_end_clean();
-                            header("Location: $url");
-                            exit();
-
                         }
+
+                        $url = BASE_URL . 'cart/cart.php';
+                        mysqli_free_result($r);
+                        mysqli_close($dbc);
+                        ob_end_clean();
+                        header("Location: $url");
+                        exit();
+
                     } else {
 
-                        $_SESSION['cart_item'] = $items_cart;
+                        $_SESSION['cart_item'] = array_merge($_SESSION['cart_item'], $items_cart);
 
                         $url = BASE_URL . 'cart/cart.php';
                         mysqli_free_result($r);
@@ -100,6 +86,16 @@ if (preg_match('/((\badd\b)|(\bdelete\b)|(\blater\b)|(\bomit\b)|(\bmove\b))(?!;)
                         exit();
 
                     }
+                } else {
+
+                    $_SESSION['cart_item'] = $items_cart;
+
+                    $url = BASE_URL . 'cart/cart.php';
+                    mysqli_free_result($r);
+                    mysqli_close($dbc);
+                    ob_end_clean();
+                    header("Location: $url");
+                    exit();
 
                 }
 
