5814e780f71f6e97286bed5901e4584a3e909d63
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-14 18:49:23 -0700

    Created new class breadcrumb for account option

--- includes/breadcrumb.php
+++ includes/breadcrumb.php
@@ -14,6 +14,9 @@
 // ************ DOCUMENTATION ************ //
 // *************************************** //
 
+// ************************************************ //
+// ************ NON-ACCOUNT BREADCRUMB ************ //
+
 class breadcrumb
 {
     private $breadcrumb;
@@ -49,5 +52,51 @@ class breadcrumb
         return $this->breadcrumb;
     }
 }
+
+// ************ NON-ACCOUNT BREADCRUMB ************ //
+// ************************************************ //
+
+
+// ************ ACCOUNT BREADCRUMB ************ //
+// ******************************************** //
+
+class account_breadcrumb
+{
+    private $breadcrumb;
+
+    private $separator = ' | ';
+
+    private $domain = 'http://localhost/FilmIndustry/eCommerce/account';
+
+    public function build($array)
+    {
+        $breadcrumbs = array_merge(array('Account' => 'profile.php'), $array);
+
+        $count = 0;
+
+        foreach($breadcrumbs as $title => $link) {
+
+            if (empty($link)) {
+
+                $this->breadcrumb .= '<span itemscope="" itemtype="https://schema.org/breadcrumb"><span class="main-location" itemprop="title">' . $title . '</span></span></span>';
+
+            } else {
+
+                $this->breadcrumb .= '<span itemscope="" itemtype="https://schema.org/breadcrumb"><a href="' . $this->domain . '/' . $link . '" itemprop="url"><span itemprop="title">' . $title . '</span></a></span>';
+
+            }
+
+            $count++;
+
+            if ($count !== count($breadcrumbs)) {
+                $this->breadcrumb .= $this->separator;
+            }
+        }
+        return $this->breadcrumb;
+    }
+}
+
+// ************ ACCOUNT BREADCRUMB ************ //
+// ******************************************** //
 
 ?>
