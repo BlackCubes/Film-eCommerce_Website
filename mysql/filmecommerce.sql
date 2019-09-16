6984cbea0563db351fde9f08a004dc9b96f72dc7
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-15 19:51:55 -0700

    Added 1 cinematographer in DB

--- mysql/filmecommerce.sql
+++ mysql/filmecommerce.sql
@@ -549,7 +549,8 @@ INSERT INTO `dps` (`id`, `first_name`, `middle_name`, `last_name`, `gender`, `ab
 (78, 'Roger', '', 'Pratt', 'M', '', 'dp/roger-pratt_Cover.jpg'),
 (79, 'Michael', '', 'Seresin', 'M', '', 'dp/michael-seresin_Cover.jpg'),
 (80, 'Slawomir', '', 'Idziak', 'M', '', 'dp/slawomir-idziak_Cover.jpg'),
-(81, 'Eduardo', '', 'Serra', 'M', '', 'dp/eduardo-serra_Cover.jpg');
+(81, 'Eduardo', '', 'Serra', 'M', '', 'dp/eduardo-serra_Cover.jpg'),
+(82, 'Barry', '', 'Ackroyd', 'M', '', 'dp/barry-ackroyd_Cover.jpg');
 
 -- --------------------------------------------------------
 
