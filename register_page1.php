d833fd55b8c22f0ab5ddb91af4aa438232ef4deb
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-17 12:39:29 -0700

    Added account container div tags

--- register_page1.php
+++ register_page1.php
@@ -127,67 +127,67 @@ if (!isset($_SESSION)) {
 
 } // End of the main Submit conditional.*/
 ?>
-
-<!-- Making the registration form as a POST method: -->
-<form action="register_page2.php" method="post" id="registerUserCard1">
-    <fieldset class="register-card">
-        <div class="register-title">
-            <h2>Create Account</h2>
-        </div>
-        <div class="register-name">
-            <div class="first-name--register">
-                <label for="registerFirstName">First Name</label>
-                <input type="text" id="registerFirstName" class="user-first-name--new" name="first_name" maxlength="20" value="<?php if (isset($_SESSION['fn'])) echo $_SESSION['fn']; unset($_SESSION['fn']); ?>">
+<div class="account-container">
+    <form action="register_page2.php" method="post" id="registerUserCard1">
+        <fieldset class="register-card">
+            <div class="register-title">
+                <h2>Create Account</h2>
             </div>
-            <div class="last-name--register">
-                <label for="registerLastName">Last Name</label>
-                <input type="text" id="registerLastName" class="user-last-name--new" name="last_name" maxlength="40" value="<?php if (isset($_SESSION['ln'])) echo $_SESSION['ln']; unset($_SESSION['ln']); ?>">
+            <div class="register-name">
+                <div class="first-name--register">
+                    <label for="registerFirstName">First Name</label>
+                    <input type="text" id="registerFirstName" class="user-first-name--new" name="first_name" maxlength="20" value="<?php if (isset($_SESSION['fn'])) echo $_SESSION['fn']; unset($_SESSION['fn']); ?>">
+                </div>
+                <div class="last-name--register">
+                    <label for="registerLastName">Last Name</label>
+                    <input type="text" id="registerLastName" class="user-last-name--new" name="last_name" maxlength="40" value="<?php if (isset($_SESSION['ln'])) echo $_SESSION['ln']; unset($_SESSION['ln']); ?>">
+                </div>
             </div>
-        </div>
-        <?php
+            <?php
 
-        if (isset($_SESSION['fnErr'])) {
-            echo '<div class="name-error"><p>' . $_SESSION['fnErr'] . '</p></div>';
-            unset($_SESSION['fnErr']);
-        }
-        
-        if (isset($_SESSION['lnErr'])) {
-            echo '<div class="name-error"><p>' . $_SESSION['lnErr'] . '</p></div>';
-            unset($_SESSION['lnErr']);
-        }
+            if (isset($_SESSION['fnErr'])) {
+                echo '<div class="name-error"><p>' . $_SESSION['fnErr'] . '</p></div>';
+                unset($_SESSION['fnErr']);
+            }
+            
+            if (isset($_SESSION['lnErr'])) {
+                echo '<div class="name-error"><p>' . $_SESSION['lnErr'] . '</p></div>';
+                unset($_SESSION['lnErr']);
+            }
 
-        ?>
-        <div class="register-email">
-            <label for="registerEmail">Email</label>
-            <input type="email" id="registerEmail" class="user-email--register" name="email" maxlength="80" value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email']; unset($_SESSION['email']); ?>">
-        </div>
-        <?php if (isset($_SESSION['emailErr'])) echo '<div class="email-error"><p>' . $_SESSION['emailErr'] . '</p></div>';
-        unset($_SESSION['emailErr']); ?>
-        <div class="register-new-password">
-            <label for="registerPassword">Password <span class="register-password-info">(at least 10 characters long)</span></label>
-            <input type="password" id="registerPassword" class="user-new-pass--register" name="password1" maxlength="255">
-        </div>
-        <?php if (isset($_SESSION['passErr1'])) echo '<div class="pass-error"><p>' . $_SESSION['passErr1'] . '</p></div>';
-        unset($_SESSION['passErr1']); ?>
-        <div class="register-confirm-password">
-            <label for="registerConfirmPassword">Confirm Password</label>
-            <input type="password" id="registerConfirmPassword" class="user-confirm-pass--register" name="password2" maxlength="255">
-        </div>
-        <?php if (isset($_SESSION['passErr2'])) echo '<div class="pass-error"><p>' . $_SESSION['passErr2'] . '</p></div>';
-        unset($_SESSION['passErr2']); ?>
-        <div class="register-continue">
-            <input type="submit" class="register-continue-button" name="continue" value="CONTINUE">
-        </div>
-        <?php if (isset($_SESSION['systemErr1'])) echo '<div class="system-error"><p>' . $_SESSION['systemErr1'] . '</p></div>';
-        unset($_SESSION['systemErr1']);
-        /*if (isset($_SESSION['systemErr2'])) echo '<p>' . $_SESSION['systemErr2'] . '</p></div>';
-        unset($_SESSION['systemErr2']);*/ ?>
-        <div class="register-sign-in-option">
-            <p>Already have an account? <a href="/FilmIndustry/eCommerce/login.php">Sign in</a></p>
-        </div>
-        <div class="register-seller-option">
-            <p>Want to sell your films? Register <a href="/FilmIndustry/eCommerce/suppliers/register.php">here</a></p>
-        </div>
-    </fieldset>
-</form>
+            ?>
+            <div class="register-email">
+                <label for="registerEmail">Email</label>
+                <input type="email" id="registerEmail" class="user-email--register" name="email" maxlength="80" value="<?php if (isset($_SESSION['email'])) echo $_SESSION['email']; unset($_SESSION['email']); ?>">
+            </div>
+            <?php if (isset($_SESSION['emailErr'])) echo '<div class="email-error"><p>' . $_SESSION['emailErr'] . '</p></div>';
+            unset($_SESSION['emailErr']); ?>
+            <div class="register-new-password">
+                <label for="registerPassword">Password <span class="register-password-info">(at least 10 characters long)</span></label>
+                <input type="password" id="registerPassword" class="user-new-pass--register" name="password1" maxlength="255">
+            </div>
+            <?php if (isset($_SESSION['passErr1'])) echo '<div class="pass-error"><p>' . $_SESSION['passErr1'] . '</p></div>';
+            unset($_SESSION['passErr1']); ?>
+            <div class="register-confirm-password">
+                <label for="registerConfirmPassword">Confirm Password</label>
+                <input type="password" id="registerConfirmPassword" class="user-confirm-pass--register" name="password2" maxlength="255">
+            </div>
+            <?php if (isset($_SESSION['passErr2'])) echo '<div class="pass-error"><p>' . $_SESSION['passErr2'] . '</p></div>';
+            unset($_SESSION['passErr2']); ?>
+            <div class="register-continue">
+                <input type="submit" class="register-continue-button" name="continue" value="CONTINUE">
+            </div>
+            <?php if (isset($_SESSION['systemErr1'])) echo '<div class="system-error"><p>' . $_SESSION['systemErr1'] . '</p></div>';
+            unset($_SESSION['systemErr1']);
+            /*if (isset($_SESSION['systemErr2'])) echo '<p>' . $_SESSION['systemErr2'] . '</p></div>';
+            unset($_SESSION['systemErr2']);*/ ?>
+            <div class="register-sign-in-option">
+                <p>Already have an account? <a href="/FilmIndustry/eCommerce/login.php">Sign in</a></p>
+            </div>
+            <div class="register-seller-option">
+                <p>Want to sell your films? Register <a href="/FilmIndustry/eCommerce/suppliers/register.php">here</a></p>
+            </div>
+        </fieldset>
+    </form>
+</div>
 <?php include('includes/footer.html'); // Include the footer. ?>
