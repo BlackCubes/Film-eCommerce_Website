6df64e14be120bc12a10fa7aa552572d0e56daad
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-22 20:28:08 -0700

    Added validations and option variables for option sidebar

--- tv/index.php
+++ tv/index.php
@@ -26,6 +26,52 @@ include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/breadcrumb.p
 
 require(MYSQL);
 
+if (isset($_GET['type'], $_GET['name'])) {
+    if (preg_match('/((\bgenre\b)|(\bactor\b)|(\bdirector\b)|(\bwriter\b)|(\bproducer\b)|(\bdp\b)|(\bstudio\b))(?!;)/', $_GET['type'])) {
+
+        $id_decrypt = urldecode(my_decrypt($_GET['name'], KEY));
+
+        if (is_numeric($id_decrypt) && preg_match('/^[1-9]{1}([0-9]{1,10})?$/', $id_decrypt)) {
+
+            $column_type = mysqli_real_escape_string($dbc, sanitize_input($_GET['type']));
+            $table_type = mysqli_real_escape_string($dbc, sanitize_input($_GET['type']) . 's');
+
+            $id = $id_decrypt;
+
+            $q = "SELECT id FROM `{$table_type}` WHERE id={$id}";
+            $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
+            $selected_id = mysqli_fetch_array($r, MYSQLI_ASSOC);
+
+            if ((mysqli_num_rows($r) == 1) && ($selected_id['id'] == $id)) {
+
+                $join_table = "JOIN `products_{$table_type}` pa ON p.id=pa.product_id";
+
+                $and_where = "AND pa.{$column_type}_id={$id}";
+
+            } else {
+                $url = BASE_URL . 'index.php';
+                ob_end_clean();
+                header("Location: $url");
+                exit();
+            }
+
+        } else {
+            $url = BASE_URL . 'index.php';
+            ob_end_clean();
+            header("Location: $url");
+            exit();
+        }
+
+    } else {
+        $url = BASE_URL . 'index.php';
+        ob_end_clean();
+        header("Location: $url");
+        exit();
+    }
+} else {
+    $join_table = $and_where = '';
+}
+
 $breadcrumb = new breadcrumb();
 ?>
 
