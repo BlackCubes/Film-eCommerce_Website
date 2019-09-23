0737aa2fa1f9f4be6c871e9c56d3d6c9b865b434
Elias Gutierrez <gutierrezelias1991@gmail.com>
2019-09-23 15:52:48 -0700

    Added format_url to side-bar link option page

--- tv/index.php
+++ tv/index.php
@@ -140,7 +140,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=actor&name=' . $actor_id_encrypt . '">' . $actor['actor_fn'] . ' ' . $actor['actor_mn'] . ' ' . $actor['actor_ln'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=actor">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=actor">See more</a></div>';
 
         $q = "SELECT DISTINCT dir.id AS director_id, dir.first_name AS director_fn, dir.middle_name AS director_mn, dir.last_name AS director_ln FROM directors AS dir JOIN products_directors AS pdir ON dir.id=pdir.director_id JOIN products AS p ON pdir.product_id=p.id JOIN departments AS d ON p.department_id=d.id WHERE d.department='TV' ORDER BY dir.last_name LIMIT 5";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
@@ -151,7 +151,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=director&name=' . $director_id_encrypt . '">' . $director['director_fn'] . ' ' . $director['director_mn'] . ' ' . $director['director_ln'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=director">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=director">See more</a></div>';
 
         $q = "SELECT DISTINCT w.id AS writer_id, w.first_name AS writer_fn, w.middle_name AS writer_mn, w.last_name AS writer_ln FROM writers AS w JOIN products_writers AS pw ON w.id=pw.writer_id JOIN products AS p ON pw.product_id=p.id JOIN departments AS d ON p.department_id=d.id WHERE d.department='TV' ORDER BY w.last_name LIMIT 5";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
@@ -162,7 +162,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=writer&name=' . $writer_id_encrypt . '">' . $writer['writer_fn'] . ' ' . $writer['writer_mn'] . ' ' . $writer['writer_ln'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=writer">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=writer">See more</a></div>';
 
         $q = "SELECT DISTINCT pro.id AS producer_id, pro.first_name AS producer_fn, pro.middle_name AS producer_mn, pro.last_name AS producer_ln FROM producers AS pro JOIN products_producers AS ppro ON pro.id=ppro.producer_id JOIN products AS p ON ppro.product_id=p.id JOIN departments AS d ON p.department_id=d.id WHERE d.department='TV' ORDER BY pro.last_name LIMIT 5";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
@@ -173,7 +173,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=producer&name=' . $producer_id_encrypt . '">' . $producer['producer_fn'] . ' ' . $producer['producer_mn'] . ' ' . $producer['producer_ln'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=producer">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=producer">See more</a></div>';
 
         $q = "SELECT DISTINCT dp.id AS dp_id, dp.first_name AS dp_fn, dp.middle_name AS dp_mn, dp.last_name AS dp_ln FROM dps AS dp JOIN products_dps AS pdp ON dp.id=pdp.dp_id JOIN products AS p ON pdp.product_id=p.id JOIN departments AS d ON p.department_id=d.id WHERE d.department='TV' ORDER BY dp.last_name LIMIT 5";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
@@ -184,7 +184,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=dp&name=' . $dp_id_encrypt . '">' . $dp['dp_fn'] . ' ' . $dp['dp_mn'] . ' ' . $dp['dp_ln'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=dp">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=dp">See more</a></div>';
 
         $q = "SELECT DISTINCT stu.id AS studio_id, stu.studio_name AS studio_name FROM studios AS stu JOIN products_studios AS pstu ON stu.id=pstu.studio_id JOIN products AS p ON pstu.product_id=p.id JOIN departments AS d ON p.department_id=d.id WHERE d.department='TV' ORDER BY stu.studio_name LIMIT 5";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error " . mysqli_error($dbc));
@@ -195,7 +195,7 @@ $breadcrumb = new breadcrumb();
 
             echo '<p><a href="/FilmIndustry/eCommerce/tv/index.php?type=studio&name=' . $studio_id_encrypt . '">' . $studio['studio_name'] . '</a></p>';
         }
-        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=DVD&type=studio">See more</a></div>';
+        echo '<a href="/FilmIndustry/eCommerce/products/other.php?department=TV&format=' . $format_to_url . '&type=studio">See more</a></div>';
 
         ?>
     </div>
