c7842dd14246f3ce1da7cd805a5f07e5ef596703
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 14:26:55 -0700

    Added documentation, page title, and header and footer files

---
+++ pages/tax-information.php
@@ -0,0 +1,11 @@
+<?php
+
+# tax-information.php (Tax Information).
+# Created on October 19, 2019.
+# Created by Elias Gutierrez
+
+$page_title = 'Tax Information';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
