9b184af8aa5c3129b42ded1cd1e7132dc504e6b0
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 16:44:55 -0700

    Added documentation, page title, and header and footer files

---
+++ pages/returns-exchanges.php
@@ -0,0 +1,11 @@
+<?php
+
+# returns-exchanges.php (Returns and Exchanges info).
+# Created on October 19, 2019.
+# Created by Elias Gutierrez.
+
+$page_title = 'Returns & Exchanges';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
