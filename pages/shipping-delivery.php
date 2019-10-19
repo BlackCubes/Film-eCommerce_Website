df0a1b47053f7b371cb3df72c986bf937beb99cc
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 08:31:51 -0700

    Created 80% of the page content

--- pages/shipping-delivery.php
+++ pages/shipping-delivery.php
@@ -8,4 +8,23 @@ $page_title = 'Shipping & Delivery';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 ?>
+<div class="pages-container">
+    <div class="grid">
+        <div class="section-header">
+            <h1>Shipping & Delivery</h1>
+        </div>
+        <div class="section-body">
+            <div class="shipping-deliver-table"></div>
+            <p>&nbsp;</p>
+            <p>*Does not apply to standees, a small number of heavy items that have a surcharge, charity items, or taxes. Valid for the contiguous 48 states, not including Alaska, Hawaii, Military, and US territories. Offer may be withdrawn or modified at any time.</p>
+            <h4>Shipping Carriers</h4>
+            <p>We use several shipping providers – USPS, UPS, FedEx, and OSM.</p>
+            <p><strong>Standard Shipping:</strong><span>&nbsp;</span>UPS Ground, U.S. Postal Service, OSM, or FedEx Ground may be used. We cannot determine at the time of your purchase what carrier will be used, so we strongly recommend you use a shipping address that can receive both USPS and UPS/FedEx deliveries.</p>
+            <p><strong>Two-Day Shipping:</strong><span>&nbsp;</span>UPS or FedEx. Please note there is no weekend or holiday delivery.</p>
+            <p><strong>Military addresses:</strong><span>&nbsp;</span>USPS First Class or Priority Mail. Please make sure to use USA as the country for military addresses.</p>
+            <p><strong>Canada:</strong><span>&nbsp;</span>We use Frontier, USPS, and Canadian Post. Tracking numbers are currently not available.</p>
+            <p><strong>P.O. Boxes:</strong><strong>&#65279;</strong>&#65279; We cannot accept P.O. Boxes as a shipping address, except military addresses. If you do not receive U.S. Mail at your home address, please be aware that if you select standard shipping, your package may be shipped with USPS.</p>
+        </div>
+    </div>
+</div>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
