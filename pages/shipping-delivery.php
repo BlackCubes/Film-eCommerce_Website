d390d3c2426e39c891815b8931eb0bf2e4e43eec
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 08:08:22 -0700

    Added documentation, page title, and the header and footer files

---
+++ pages/shipping-delivery.php
@@ -0,0 +1,11 @@
+<?php
+
+# shipping-delivery.php (Shipping & Delivery info).
+# Created on October 19, 2019.
+# Created by Elias Gutierrez
+
+$page_title = 'Shipping & Delivery';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
