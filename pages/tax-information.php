e4eea904c3f788acf7ce17a1d2230eb46900d74f
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 15:57:29 -0700

    Added info for the Tax Information page

--- pages/tax-information.php
+++ pages/tax-information.php
@@ -8,4 +8,18 @@ $page_title = 'Tax Information';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 ?>
+<div class="pages-container">
+    <div class="grid">
+        <div class="section-header">
+            <h1>Tax Information</h1>
+        </div>
+        <div class="section-body">
+            <h4>Sales Tax</h4>
+            <br>
+            <p>For applicable purchases, we collect and remit applicable sales and use tax in all USA states except for Maine, Mississippi, North Dakota, South Dakota, West Virginia, and Wyoming.</p>
+            <p>Where we do not collect sales tax from you upon your purchase, your purchase may still be subject to use tax and is not exempt merely because it is made over the Internet or by other remote means. Many states require purchasers to file a sales/use tax return at the end of the year reporting all of their taxable purchases that were not taxed and to pay tax on those purchases. Details of how to file these returns may be found at the websites of your respective taxing authorities.</p>
+            <p>If you are a tax exempt organization such as a charity or school, please contact us for assistance after placing your order.</p>
+        </div>
+    </div>
+</div>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
