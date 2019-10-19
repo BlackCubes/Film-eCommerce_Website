b2dcd7286895bc07e3e983b4a49fa54a0ca1f1a2
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 08:51:39 -0700

    Created shipping-info table

--- pages/shipping-delivery.php
+++ pages/shipping-delivery.php
@@ -14,7 +14,89 @@ include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html'
             <h1>Shipping & Delivery</h1>
         </div>
         <div class="section-body">
-            <div class="shipping-deliver-table"></div>
+            <div class="shipping-deliver-table">
+                <table width="100%">
+                    <tbody>
+                        <tr>
+                            <td>&nbsp;Order Total</td>
+                            <td>Standard</td>
+                            <td>Two-Day</td>
+                            <td>AK, HI, Military, and U.S. Territories</td>
+                            <td>Canada</td>
+                        </tr>
+                        <tr>
+                            <td>$0.00 - $29.99</td>
+                            <td>$5.99</td>
+                            <td>$16.99</td>
+                            <td>$8.99</td>
+                            <td>$10.99</td>
+                        </tr>
+                        <tr>
+                            <td>$25.00 - $39.99</td>
+                            <td>$6.99</td>
+                            <td>$17.99</td>
+                            <td>$9.99</td>
+                            <td>$11.99</td>
+                        </tr>
+                        <tr>
+                            <td>$40.00 - $49.99</td>
+                            <td>$7.99</td>
+                            <td>$18.99</td>
+                            <td>$10.99</td>
+                            <td>$12.99</td>
+                        </tr>
+                        <tr>
+                            <td>$50.00 - $59.99</td>
+                            <td>$8.99</td>
+                            <td>$19.49</td>
+                            <td>$11.49</td>
+                            <td>$13.49</td>
+                        </tr>
+                        <tr>
+                            <td>$60.00 - $69.99</td>
+                            <td>$9.99</td>
+                            <td>$19.99</td>
+                            <td>$11.99</td>
+                            <td>$13.99</td>
+                        </tr>
+                        <tr>
+                            <td>$70.00 - $70.99</td>
+                            <td>$10.99</td>
+                            <td>$20.49</td>
+                            <td>$12.49</td>
+                            <td>$14.49</td>
+                        </tr>
+                        <tr>
+                            <td>$80.00 - $80.99</td>
+                            <td>$11.99</td>
+                            <td>$20.99</td>
+                            <td>$12.99</td>
+                            <td>$14.99</td>
+                        </tr>
+                        <tr>
+                            <td>$90.00 - $90.99</td>
+                            <td>$12.99</td>
+                            <td>$22.99</td>
+                            <td>$14.99</td>
+                            <td>$15.99</td>
+                        </tr>
+                        <tr>
+                            <td>$100.00 - $299.99</td>
+                            <td>FREE*</td>
+                            <td>$25.99</td>
+                            <td>$16.99</td>
+                            <td>$17.99</td>
+                        </tr>
+                        <tr>
+                            <td>$300.00+</td>
+                            <td>FREE*</td>
+                            <td>$29.99</td>
+                            <td>$19.99</td>
+                            <td>$24.99</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <p>&nbsp;</p>
             <p>*Does not apply to standees, a small number of heavy items that have a surcharge, charity items, or taxes. Valid for the contiguous 48 states, not including Alaska, Hawaii, Military, and US territories. Offer may be withdrawn or modified at any time.</p>
             <h4>Shipping Carriers</h4>
