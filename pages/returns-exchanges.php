701db898d5bc79dfeefa51c4e7dba2a695a8049e
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 17:02:03 -0700

    Added the rest of the page info

--- pages/returns-exchanges.php
+++ pages/returns-exchanges.php
@@ -8,4 +8,47 @@ $page_title = 'Returns & Exchanges';
 
 include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
 ?>
+<div class="pages-container">
+    <div class="grid">
+        <div class="section-header">
+            <h1>Returns & Exchanges</h1>
+        </div>
+        <div class="section-body">
+            <p>Although you cannot actually buy anything at the website, at (Website), our goal is your complete satisfaction. You may return any new item for any reason within 30 days for a refund. The item must be in the original, unopened package, to be accepted back for a refund.</p>
+            <h4>How to Return an Item</h4>
+            <p>
+                If an item is damaged, defective, or incorrect, contact us and we will issue a Return Authorization. If the item is not damaged, defective, or incorrect, please include a copy of the packing slip and write the reason for the return, and if you would like a refund or an exchange.
+                <br>
+                <br>
+                You will be responsible for the cost of shipping an item back to us. We recommend using carrier that provides you with a tracking number, such as UPS, FedEx, or USPS Priority Mail.
+                <br>
+                <br>
+                <strong>PLEASE NOTE: YOU CANNOT ACTUALLY BUY ANY PRODUCTS ON THIS WEBSITE! IT IS A PROJECT INTENDED TO DISPLAY SKILLSETS! BUT, FOR FUTURE REFERENCES AND POSSIBILITIES, ONLY MERCHANDISE PURCHASED DIRECTLY FROM (WEBSITE) CAN BE RETURNED TO THE FOLLOWING ADDRESS BELOW. WE CANNOT ACCEPT ANY ITEMS PURCHASED AT ANY OTHER RETAILERS.</strong>
+            </p>
+            <p>
+                Please return Consumer Products to:
+                <br>
+                <br>
+                (Website)
+                <br>
+                Attn: Returns
+                <br>
+                Company Address
+                <br>
+                Company City, Company State Company Zipcode
+                <br>
+                United States
+                <br>
+                <br>
+                <strong>Customer Service:</strong>
+                <br>
+                Phone: 559-595-3839
+                <br>
+                3pm-8pm PST Monday-Friday
+                <br>
+                10am-8pm PST Saturday & Sunday.
+            </p>
+        </div>
+    </div>
+</div>
 <?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
