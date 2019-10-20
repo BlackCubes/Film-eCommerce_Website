222cfa8da94280041dc94415e8f5b644b7cf2544
BlackCubes <gutierrezelias1991@gmail.com>
2019-10-19 17:59:12 -0700

    Added documentation, page title, and header and footer files

---
+++ pages/volume-discounts.php
@@ -0,0 +1,11 @@
+<?php
+
+# volume-discounts.php (Volume Discounts info).
+# Created on October 19, 2019.
+# Created by Elias Gutierrez.
+
+$page_title = 'Volume Discounts';
+
+include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/header.html');
+?>
+<?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>
