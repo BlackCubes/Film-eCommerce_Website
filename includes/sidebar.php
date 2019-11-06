8bd3ff61ff03af6b5b6b80d2a4db667f6a49a07d
BlackCubes <gutierrezelias1991@gmail.com>
2019-11-06 08:17:21 -0800

    Created and initialized the documentation

---
+++ includes/sidebar.php
@@ -0,0 +1,24 @@
+<?php
+
+# sidebar.php (File to show the Sidebar on Desktop or Mobile Responsive).
+# Created on November 6, 2019.
+# Created by Elias Gutierrez.
+
+// ***************************************** //
+// ************ DESKTOP SIDEBAR ************ //
+
+
+
+// ************ DESKTOP SIDEBAR ************ //
+// ***************************************** //
+
+
+// **************************************** //
+// ************ MOBILE SIDEBAR ************ //
+
+
+
+// ************ MOBILE SIDEBAR ************ //
+// **************************************** //
+
+?>
