7ee031914d10fd5f2a3e877a174bdd31cfb56018
BlackCubes <gutierrezelias1991@gmail.com>
2019-11-15 15:49:02 -0800

    Deleted old codes

--- register_page2.php
+++ register_page2.php
@@ -82,96 +82,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     if ($fn && $ln && $e && $p) {
 
-        $_SESSION['systemErr1'] = ''; /*$_SESSION['systemErr2'] = '';*/
+        $_SESSION['systemErr1'] = '';
 
         $q = "SELECT id FROM users WHERE email='{$_SESSION['email']}'";
         $r = mysqli_query($dbc, $q) or trigger_error("Query: $q\n<br>MySQL Error: " . mysqli_error($dbc));
 
         if (mysqli_num_rows($r) == 1) {
 
-            #mysqli_close($dbc);
-
             mysqli_close($dbc);
 
             $_SESSION['systemErr1'] = 'That email address has already been registered. If you have forgotten your password, please change it by clicking on "Change Password" on the top navbar when signing in';
             $_SESSION['systemErr2'] = '';
             header("Location: http://localhost/FilmIndustry/eCommerce/register_page1.php");
             exit();
+            
+        }
 
-        /*?>
-
-            <!--<form action="register_complete.php" method="post" id="registerUserCard2">
-                <fieldset class="register-card--shipping">
-                    <div class="shipping-title--register">
-                        <h2>Shipping Address</h2>
-                    </div>
-                    <div class="shipping-info--register">
-                        <p>Please provide an address to be used for shipping.</p>
-                    </div>
-                    <div class="shipping-address-1">
-                        <label for="shippingAddress1">Shipping Address</label>
-                        <input type="text" id="shippingAddress1" class="user-address-1--register" name="address1" maxlength="46" value="<#?php if (isset($_SESSION['a1'])) echo $_SESSION['a1']; unset($_SESSION['a1']); ?>">
-                    </div>
-                    <#?php if (isset($_SESSION['a1Err'])) echo '<div class="address-1-error--register"><p>' . $_SESSION['a1Err'] . '</p></div>';
-                    unset($_SESSION['a1Err']); ?>
-                    <div class="shipping-address-2">
-                        <label for="shippingAddress2">Apartment / Suite / Other <span class="register-shipping-info">(optional)</span></label>
-                        <input type="text" id="shippingAddress2" class="user-address-2--register" name="address2" maxlength="46" value="<#?php if (isset($_SESSION['a2'])) echo $_SESSION['a2']; unset($_SESSION['a2']); ?>">
-                    </div>
-                    <#?php if (isset($_SESSION['a2Err'])) echo '<div class="address-2-error--register"><p>' . $_SESSION['a2Err'] . '</p></div>';
-                    unset($_SESSION['a2Err']); ?>
-                    <div class="shipping-city">
-                        <label for="shippingCity">City</label>
-                        <input type="text" id="shippingCity" class="user-city--register" name="city" maxlength="50" value="<#?php if (isset($_SESSION['c'])) echo $_SESSION['c']; unset($_SESSION['c']); ?>">
-                    </div>
-                    <#?php if (isset($_SESSION['cErr'])) echo '<div class="city-error--register"><p>' . $_SESSION['cErr'] . '</p></div>';
-                    unset($_SESSION['cErr']); ?>
-                    <div class="shipping-state-zip">
-                        <div class="shipping-state">
-                            <label for="shippingState">State</label>
-                            <input type="text" id="shippingState" class="user-state--register" name="state" maxlength="50" placeholder="NJ or New Jersey" value="<#?php if (isset($_SESSION['s'])) echo $_SESSION['s']; unset($_SESSION['s']); ?>">
-                        </div>
-                        <div class="shipping-zip-code">
-                            <label for="shippingZipCode">Zip Code</label>
-                            <input type="text" id="shippingZipCode" class="user-zip--register" name="zip" maxlength="10" placeholder="12345 or 12345-6789" value="<#?php if (isset($_SESSION['z'])) echo $_SESSION['z']; unset($_SESSION['z']); ?>">
-                        </div>
-                    </div>
-                    <#?php
-
-                    if (isset($_SESSION['sErr'])) echo '<div class="state-error--register"><p>' . $_SESSION['sErr'] . '</p></div>';
-                    unset($_SESSION['sErr']);
-
-                    if (isset($_SESSION['zipErr'])) echo '<div class="zip-code-error--register"><p>' . $_SESSION['zipErr'] . '</p></div>';
-                    unset($_SESSION['zipErr']);
-
-                    ?>
-                    <div class="shipping-phone">
-                        <label for="shippingPhone">Phone <span class="register-phone-info">(optional, but may be used to assist delivery)</span></label>
-                        <input type="tel" id="shippingPhone" class="user-phone--register" name="phone_num" placeholder="123-456-7890" maxlength="15" value="<#?php if (isset($_SESSION['phone_num'])) echo $_SESSION['phone_num']; unset($_SESSION['phone_num']); ?>">
-                    </div>
-                    <#?php if (isset($_SESSION['phoneErr'])) echo '<div class="phone-error--register"><p>' . $_SESSION['phoneErr'] . '</p></div>';
-                    unset($_SESSION['phoneErr']); ?>
-                    <div class="register-submit">
-                        <input type="submit" class="submit-button--register" name="submit" value="OK, LET'S GO!">
-                    </div>
-                    <#?php if (isset($_SESSION['systemErr2'])) echo '<div class="system-error"><p>' . $_SESSION['systemErr2'] . '</p></div>'; unset($_SESSION['systemErr2']); ?>
-                </fieldset>
-            </form>
-            <#?php include($_SERVER['DOCUMENT_ROOT'].'/FilmIndustry/eCommerce/includes/footer.html'); ?>-->
-
-        <?php*/
-        } /*else {
-
-            mysqli_close($dbc);
-
-            $_SESSION['systemErr1'] = 'That email address has already been registered. If you have forgotten your password, please change it by clicking on "Change Password" on the top navbar when signing in';
-            header("Location: http://localhost/FilmIndustry/eCommerce/register_page1.php");
-            $_SESSION['systemErr2'] = '';
-            exit();
-        }*/
     } else {
         $_SESSION['systemErr1'] = 'Please try again';
-        /*$_SESSION['systemErr2'] = '';*/
         header("Location: http://localhost/FilmIndustry/eCommerce/register_page1.php");
         exit();
     }
