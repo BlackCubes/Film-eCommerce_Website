9d5f3a83de3e0bcddaafbc4fa89ed5bf5320ee48
BlackCubes <gutierrezelias1991@gmail.com>
2019-11-17 20:34:45 -0800

    Added account header and footer

--- activate.php
+++ activate.php
@@ -9,7 +9,7 @@ require('includes/config.inc.php');
 
 $page_title = 'Activate Your Account';
 
-include('includes/header.html');
+include('includes/account_header.html');
 
 if (isset($_GET['x'], $_GET['y']) && filter_var($_GET['x'], FILTER_VALIDATE_EMAIL) && (strlen($_GET['y']) == 32)) {
 
@@ -37,5 +37,5 @@ if (isset($_GET['x'], $_GET['y']) && filter_var($_GET['x'], FILTER_VALIDATE_EMAI
 
 }
 
-include('includes/footer.html');
+include('includes/account_footer.html');
 ?>
